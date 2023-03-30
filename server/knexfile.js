07d37693363910062c5fe25630460dc8ec3701d1
Gino Ochoa <gochoa1088@gmail.com>
2023-03-30 15:48:24 -0700

    initial knex setup

---
+++ server/knexfile.js
@@ -0,0 +1,34 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "pg",
+    connection: process.env.DB_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};

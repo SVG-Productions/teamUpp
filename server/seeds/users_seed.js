5dedbbab6187fc04296f1e51dff0bb0b41109790
Gino Ochoa <gochoa1088@gmail.com>
2023-03-31 12:10:10 -0700

    use appropriate tables in seed files

--- server/seeds/users_seed.js
+++ server/seeds/users_seed.js
@@ -1,13 +1,13 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex("users").del();
+  await knex("users").insert([
+    { id: 1, colName: "rowValue1" },
+    { id: 2, colName: "rowValue2" },
+    { id: 3, colName: "rowValue3" },
   ]);
 };

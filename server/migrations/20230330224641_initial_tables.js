8540b8ba899d89f051075f8e45475a470098c7e3
Gino Ochoa <gochoa1088@gmail.com>
2023-03-30 17:46:10 -0700

    initial table migrations

--- server/migrations/20230330224641_initial_tables.js
+++ server/migrations/20230330224641_initial_tables.js
@@ -2,14 +2,86 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = async function (knex) {
+  await knex.schema.createTable("users", function (table) {
+    table.uuid("id").defaultTo(knex.raw("gen_random_uuid()")).primary();
+    table.string("username").notNullable();
+    table.string("email").notNullable();
+    table.boolean("is_email_public").defaultTo(false);
+    table.string("hashed_password", 100).notNullable();
+    table.string("first_name", 100);
+    table.string("last_name", 100);
+    table.timestamp("date_joined").defaultTo(knex.fn.now());
+    table.string("linkedin");
+    table.string("github");
+    table.text("readme");
+    table.text("team_ids");
+    table.text("listing_ids");
+    table.text("favorite_ids");
+  });
+  await knex.schema.createTable("teams", function (table) {
+    table.uuid("id").defaultTo(knex.raw("gen_random_uuid()")).primary();
+    table.text("user_ids").notNullable();
+    table.text("listing_ids");
+    table.string("name").notNullable();
+    table.string("job_field");
+    table.text("description").notNullable();
+    table.boolean("isPrivate").defaultTo(false);
+  });
+  await knex.schema.createTable("listings", function (table) {
+    table.uuid("id").defaultTo(knex.raw("gen_random_uuid()")).primary();
+    table
+      .uuid("user_id")
+      .notNullable()
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table
+      .uuid("team_id")
+      .notNullable()
+      .references("id")
+      .inTable("teams")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table.text("experience_ids");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.string("job_title").notNullable();
+    table.string("job_link").notNullable();
+    table.string("company_name").notNullable();
+    table.text("company_details").notNullable();
+    table.text("job_description").notNullable();
+  });
+  await knex.schema.createTable("experiences", function (table) {
+    table.uuid("id").defaultTo(knex.raw("gen_random_uuid()")).primary();
+    table
+      .uuid("user_id")
+      .notNullable()
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table
+      .uuid("listing_id")
+      .notNullable()
+      .references("id")
+      .inTable("listings")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.string("title");
+    table.text("content");
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("users")
+    .dropTableIfExists("listings")
+    .dropTableIfExists("experiences")
+    .dropTableIfExists("teams");
 };

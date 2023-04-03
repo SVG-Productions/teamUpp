489e0ff4a6b69f9510f58e4c7638a2fce354ee5d
Schaeffer <schaefferahn@gmail.com>
2023-04-03 16:31:03 -0700

    footer done. Added link placeholders and copyright

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
-  return <div className="h-16 border-t-2">FOOTER</div>;
+  return <div className="flex flex-col h-16 border-t-2 justify-center text-center">
+    <div className='flex justify-center gap-3'>
+      <NavLink to='#'>About</NavLink> |
+      <a href="https://github.com/SVG-Productions/teamUpp" target="_blank" rel="noreferrer">GitHub</a> |
+      <NavLink to='#'>Terms & Policies</NavLink> |
+      <NavLink to='#'>Contact Us</NavLink>
+    </div>
+    <div>
+      Copyright &copy; 2023 SVG Productions
+    </div>
+  </div>;
 };
 
 export default Footer;

538380a851d97e0b89a99dc280fa149db69e902a
vincentochoa3 <vochoa893@gmail.com>
2023-04-03 21:42:04 -0700

    change AuthedPageContainer height to be percentage-based

--- client/src/components/AuthedPageContainer.js
+++ client/src/components/AuthedPageContainer.js
@@ -1,5 +1,9 @@
 const AuthedPageContainer = ({ children }) => {
-  return <div className="flex grow flex-col px-20 pt-4 pb-8">{children}</div>;
+  return (
+    <div className="flex flex-col px-20 pt-4 pb-12 h-[calc(100vh-8rem)]">
+      {children}
+    </div>
+  );
 };
 
 export default AuthedPageContainer;
--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -5,11 +5,21 @@ const UserPage = () => {
   return (
     <AuthedPageContainer>
       <AuthedPageTitle>Username</AuthedPageTitle>
-      <div className="flex gap-10 my-8 grow-[2]">
-        <div className="p-4 rounded-md w-72 bg-slate-100">User Info</div>
-        <div className="p-4 rounded-md grow bg-slate-100">User ReadME</div>
+      <div className="flex gap-10 my-8 h-2/3">
+        <div className="flex flex-col gap-8 p-4 rounded-md w-72 bg-slate-100">
+          <div className="flex items-center justify-center w-32 h-32 rounded-full bg-white">
+            Profile Pic
+          </div>
+          <div>
+            <div className="p-4">Name</div>
+            <div className="p-4">Date Joined</div>
+            <div className="p-4">LinkedIn</div>
+            <div className="p-4">Github</div>
+          </div>
+        </div>
+        <div className="p-4 w-full rounded-md bg-slate-100">User ReadME</div>
       </div>
-      <div className="flex gap-10 grow">
+      <div className="flex h-1/3 gap-10">
         <div className="p-4 rounded-md w-2/3 bg-slate-100">Teams</div>
         <div className="p-4 rounded-md w-1/3 bg-slate-100">
           All Team Members

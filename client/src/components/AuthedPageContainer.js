96cfa010d2cd8ca848b3167020d5ed33a3bc7f70
vincentochoa3 <vochoa893@gmail.com>
2023-04-03 20:56:34 -0700

    add layout and sections for UserPage

--- client/src/components/AuthedPageContainer.js
+++ client/src/components/AuthedPageContainer.js
@@ -1,9 +1,5 @@
 const AuthedPageContainer = ({ children }) => {
-  return (
-    <div className="flex grow flex-col items-stretch px-20 py-8">
-      {children}
-    </div>
-  );
+  return <div className="flex grow flex-col px-20 py-8">{children}</div>;
 };
 
 export default AuthedPageContainer;

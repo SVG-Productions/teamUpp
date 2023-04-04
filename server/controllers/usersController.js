72eebdaf291e470a2dbc87915434d2113974aa11
Gino Ochoa <gochoa1088@gmail.com>
2023-04-03 18:28:31 -0700

    create dbConfig file

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,9 +1,15 @@
-const getAllUsers = (req, res, next) => {
+const User = require("../models/User");
+
+const getAllUsers = async (req, res, next) => {
   try {
-    const users = Users.getAllUsers();
+    const users = await User.getAllUsers();
     res.status(200).json({ message: "Users fetched successfully.", users });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error fetching users.", error });
   }
+};
+
+module.exports = {
+  getAllUsers,
 };

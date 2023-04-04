5147b2271b48ee9922d78dd6b9b1b7b654f053c7
Gino Ochoa <gochoa1088@gmail.com>
2023-04-03 18:33:29 -0700

    apply getAllUsers controller to users route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,8 @@
 var express = require("express");
 var router = express.Router();
+const { getAllUsers } = require("../controllers/usersController");
 
 /* GET users. */
-router.get("/", function (req, res, next) {
-  res.json({ users: ["userOne", "userTwo", "userThree"] });
-});
+router.get("/", getAllUsers);
 
 module.exports = router;

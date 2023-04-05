d2b8c43c6f40b959af34be57dd54878000a72ed7
Gino Ochoa <gochoa1088@gmail.com>
2023-04-05 11:40:06 -0700

    change bottom container to redirect to login

--- client/src/pages/SignUpPage.js
+++ client/src/pages/SignUpPage.js
@@ -54,12 +54,12 @@ const SignUpPage = () => {
         </form>
         <div className="border-2 border-slate-300 w-full max-w-sm bg-slate-100 mb-20 rounded-lg shadow-lg p-5 text-center">
           <p className="font-semibold text-sm text-slate-600">
-            New to TeamApp?
+            Already have an account?
             <a
               className="mx-2 inline-block align-baseline font-semibold text-sm text-blue-500 hover:text-blue-800"
-              href="/signup"
+              href="/login"
             >
-              Create an account!
+              Login!
             </a>
           </p>
         </div>

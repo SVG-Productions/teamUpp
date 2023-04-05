8f2bd58d20a1dccdf1e0968d0df600bb436948f3
Gino Ochoa <gochoa1088@gmail.com>
2023-04-05 11:44:31 -0700

    change page heading

--- client/src/pages/SignUpPage.js
+++ client/src/pages/SignUpPage.js
@@ -5,7 +5,7 @@ const SignUpPage = () => {
     <>
       <div className="flex flex-col justify-center items-center min-h-[calc(100vh-4rem)]">
         <div className="border-2 border-slate-300 w-32 h-32 my-8 bg-slate-100 rounded-full" />
-        <h1 className="text-4xl text-slate-600 mb-10">Sign In to TeamApp</h1>
+        <h1 className="text-4xl text-slate-600 mb-10">Welcome to TeamApp</h1>
         <form className="border-2 border-slate-300 w-full max-w-sm mb-10 bg-slate-100 rounded-lg shadow-lg p-6">
           <div className="mb-4">
             <label

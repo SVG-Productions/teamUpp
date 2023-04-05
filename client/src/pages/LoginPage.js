3a7a391bfa12fc67660981a8fb16ecf1e0cc6a10
Gino Ochoa <gochoa1088@gmail.com>
2023-04-04 23:00:59 -0700

    initial login ui

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,5 +1,49 @@
 const LoginPage = () => {
-  return <div>LOGIN</div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <form className="w-full max-w-md bg-white rounded-lg shadow-lg p-6">
+        <h2 className="text-2xl font-bold mb-6">Login</h2>
+        <div className="mb-4">
+          <label className="block font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Email"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Password"
+            required
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign In
+          </button>
+          <a
+            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+            href="#"
+          >
+            Forgot Password?
+          </a>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;

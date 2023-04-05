4ce19c24b993acb97ef9f1ffd98146194b9ab311
Gino Ochoa <gochoa1088@gmail.com>
2023-04-04 23:16:21 -0700

    increase width of sign in button

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -4,11 +4,11 @@ const LoginPage = () => {
       <form className="w-full max-w-md bg-white rounded-lg shadow-lg p-6">
         <h2 className="text-2xl font-bold mb-6">Login</h2>
         <div className="mb-4">
-          <label className="block font-bold mb-2" htmlFor="email">
+          <label className="block font-semibold mb-2" htmlFor="email">
             Email
           </label>
           <input
-            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline"
             id="email"
             type="email"
             placeholder="Email"
@@ -17,7 +17,7 @@ const LoginPage = () => {
         </div>
         <div className="mb-6">
           <div className="flex justify-between items-center">
-            <label className="block font-bold mb-2" htmlFor="password">
+            <label className="block font-semibold mb-2" htmlFor="password">
               Password
             </label>
             <a
@@ -28,21 +28,19 @@ const LoginPage = () => {
             </a>
           </div>
           <input
-            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline"
             id="password"
             type="password"
             placeholder="Password"
             required
           />
         </div>
-        <div className="flex items-center justify-between">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
-          >
-            Sign In
-          </button>
-        </div>
+        <button
+          className="w-full bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:shadow-outline"
+          type="submit"
+        >
+          Sign In
+        </button>
       </form>
     </div>
   );

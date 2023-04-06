c71249f6017912aba2ac797867724ec28c3cf9d9
vincentochoa3 <vochoa893@gmail.com>
2023-04-06 12:45:19 -0700

    remove console.log

--- client/src/utils/formatJoinDate.js
+++ client/src/utils/formatJoinDate.js
@@ -1,6 +1,5 @@
 const formatJoinDate = (date) => {
   const month = date.toLocaleString("default", { month: "short" });
-  console.log(month);
   const year = date.getFullYear();
 
   return month + " " + year;

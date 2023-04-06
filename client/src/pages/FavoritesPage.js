59d75d6e5f7fde361ec19fd54a0668db5267d81b
Gino Ochoa <gochoa1088@gmail.com>
2023-04-06 16:59:42 -0700

    change variables in favorites to match camelCase

--- client/src/pages/FavoritesPage.js
+++ client/src/pages/FavoritesPage.js
@@ -7,29 +7,39 @@ import formatDate from "../utils/formatDate";
 
 const FavoritesPage = () => {
   const { userFavorites } = useLoaderData();
-  const { favorites } = userFavorites.data
+  const { favorites } = userFavorites.data;
 
   return (
     <>
       <AuthedPageTitle>User Favorites</AuthedPageTitle>
-      <ScrollableList title="Favorite Listings" width="w-full min-w-[325px] -mx-4 sm:mx-0" height="h-[40rem] sm:h-[100%] min-h-[430px] mt-8">
+      <ScrollableList
+        title="Favorite Listings"
+        width="w-full min-w-[325px] -mx-4 sm:mx-0"
+        height="h-[40rem] sm:h-[100%] min-h-[430px] mt-8"
+      >
         {favorites.map((listing, index) => (
           <div key={index} className="flex flex-row bg-white p-2.5 rounded-md">
             <div className="flex flex-row w-2/3 items-center">
               <FavoriteButton />
-              <div className="text-xs sm:text-lg font-bold">{listing.company_name}</div>
+              <div className="text-xs sm:text-lg font-bold">
+                {listing.companyName}
+              </div>
               <div className="hidden sm:block sm:text-lg font-bold mx-2">/</div>
-              <div className="text-xs sm:text-base px-3 sm:px-0">{listing.job_title}</div>
+              <div className="text-xs sm:text-base px-3 sm:px-0">
+                {listing.jobTitle}
+              </div>
             </div>
             <div className="flex flex-row justify-end w-1/3 items-center">
-              <div className="text-xs sm:text-sm">{formatDate(listing.created_at)}</div>
+              <div className="text-xs sm:text-sm">
+                {formatDate(listing.createdAt)}
+              </div>
               <DropdownMenuButton />
             </div>
           </div>
         ))}
       </ScrollableList>
     </>
-  )
+  );
 };
 
 export default FavoritesPage;

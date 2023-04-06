5da272f74d9a8f2e3fe8f25a25942d653270bd19
vincentochoa3 <vochoa893@gmail.com>
2023-04-06 12:33:06 -0700

    optimize formatJoinDate util

--- client/src/utils/formatJoinDate.js
+++ client/src/utils/formatJoinDate.js
@@ -1,23 +1,9 @@
 const formatJoinDate = (date) => {
-  const months = [
-    "Jan ",
-    "Feb ",
-    "Mar ",
-    "Apr ",
-    "May ",
-    "Jun ",
-    "Jul ",
-    "Aug ",
-    "Sep ",
-    "Oct ",
-    "Nov ",
-    "Dec ",
-  ];
-
-  const month = months[date.getMonth()];
+  const month = date.toLocaleString("default", { month: "short" });
+  console.log(month);
   const year = date.getFullYear();
 
-  return month + year;
+  return month + " " + year;
 };
 
 export default formatJoinDate;

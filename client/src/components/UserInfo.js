601b4790f19b52be9f37b3fc5e24a66b83120df8
vincentochoa3 <vochoa893@gmail.com>
2023-04-06 11:23:18 -0700

    add email to UserInfo if user is_email_private is false

--- client/src/components/UserInfo.js
+++ client/src/components/UserInfo.js
@@ -2,40 +2,78 @@ import NullInfo from "./NullInfo";
 import formatJoinDate from "../utils/formatJoinDate";
 
 const UserInfo = ({ user }) => {
-  const { date_joined, first_name, github, linkedin } = user;
+  const { date_joined, email, first_name, github, linkedin, is_email_public } =
+    user;
 
   const date = new Date(date_joined);
   const formattedDate = formatJoinDate(date);
 
   return (
     <>
-      <div className="sm:p-4 py-1 px-4">
-        name /{" "}
-        {first_name ? (
-          <span className="text-sm font-bold ">{first_name}</span>
-        ) : (
-          <NullInfo />
-        )}
-      </div>
-      <div className="sm:p-4 py-1 px-4">
-        joined / <span className="text-sm font-bold">{formattedDate}</span>
-      </div>
-      <div className="sm:p-4 py-1 px-4">
-        linkedIn /{" "}
-        {linkedin ? (
-          <span className="text-sm font-bold">{linkedin}</span>
-        ) : (
-          <NullInfo />
-        )}
-      </div>
-      <div className="sm:p-4 py-1 px-4">
-        github /{" "}
-        {github ? (
-          <span className="text-sm font-bold">{github}</span>
-        ) : (
-          <NullInfo />
-        )}
-      </div>
+      {is_email_public ? (
+        <>
+          <div className="sm:py-3 py-1 px-4">
+            name /{" "}
+            {first_name ? (
+              <span className="text-sm font-bold ">{first_name}</span>
+            ) : (
+              <NullInfo />
+            )}
+          </div>
+          <div className="sm:py-3 py-1 px-4">
+            joined / <span className="text-sm font-bold">{formattedDate}</span>
+          </div>
+          <div className="sm:py-3 py-1 px-4">
+            linkedIn /{" "}
+            {linkedin ? (
+              <span className="text-sm font-bold">{linkedin}</span>
+            ) : (
+              <NullInfo />
+            )}
+          </div>
+          <div className="sm:py-3 py-1 px-4">
+            github /{" "}
+            {github ? (
+              <span className="text-sm font-bold">{github}</span>
+            ) : (
+              <NullInfo />
+            )}
+          </div>
+          <div className="sm:py-3 py-1 px-4">
+            <span>{email}</span>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="sm:p-4 py-1 px-4">
+            name /{" "}
+            {first_name ? (
+              <span className="text-sm font-bold ">{first_name}</span>
+            ) : (
+              <NullInfo />
+            )}
+          </div>
+          <div className="sm:p-4 py-1 px-4">
+            joined / <span className="text-sm font-bold">{formattedDate}</span>
+          </div>
+          <div className="sm:p-4 py-1 px-4">
+            linkedIn /{" "}
+            {linkedin ? (
+              <span className="text-sm font-bold">{linkedin}</span>
+            ) : (
+              <NullInfo />
+            )}
+          </div>
+          <div className="sm:p-4 py-1 px-4">
+            github /{" "}
+            {github ? (
+              <span className="text-sm font-bold">{github}</span>
+            ) : (
+              <NullInfo />
+            )}
+          </div>
+        </>
+      )}
     </>
   );
 };

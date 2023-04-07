da0650ee2851c5a40740b56d088619962fd6ae02
Gino Ochoa <gochoa1088@gmail.com>
2023-04-07 10:18:28 -0700

    CSRF protection

--- server/app.js
+++ server/app.js
@@ -3,11 +3,14 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var csurf = require("@dr.pogodin/csurf");
+var helmet = require("helmet");
 
 var usersRouter = require("./routes/users");
 var teamsRouter = require("./routes/teams");
 var listingsRouter = require("./routes/listings");
 var experiencesRouter = require("./routes/experiences");
+const isProduction = process.env.DB_ENVIRONMENT === "production";
 
 var app = express();
 
@@ -16,6 +19,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin",
+  })
+);
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
 
 // route prefixing and useage of imported routers
 app.use("/api/users", usersRouter);

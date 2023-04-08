803372cb3c04d1ee514179c1ea80f18ec25bf737
Schaeffer <schaefferahn@gmail.com>
2023-04-08 13:48:27 -0700

    able to get users teams using the authed user id. Need to remove duplicate from all teams

--- client/src/pages/TeamPage.js
+++ client/src/pages/TeamPage.js
@@ -1,13 +1,15 @@
 import { useNavigate } from "react-router-dom";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, NavLink } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const TeamPage = () => {
   const { authedUser } = useAuth();
   const { singleTeamData } = useLoaderData();
   const navigate = useNavigate();
   const { name, jobField, description } = singleTeamData.data.team;
+  const [teams, setTeams] = useState([]);
 
   useEffect(() => {
     if (!authedUser) {
@@ -15,11 +17,35 @@ const TeamPage = () => {
     }
   }, [authedUser]);
 
+  useEffect(() => {
+    const getUserTeams = async () => {
+      const response = await axios.get(`/api/users/${authedUser.id}/teams`);
+      setTeams(response.data.teams);
+    };
+    getUserTeams();
+  }, [authedUser.id]);
+
   return (
-    <div>
-      <h1 className="text-2xl font-bold">{name}</h1>
-      <p>{jobField}</p>
-      <p>{description}</p>
+    <div className="flex flex-row justify-between">
+      <div>
+        <h1 className="text-2xl font-bold">{name}</h1>
+        <p>{jobField}</p>
+        <p>{description}</p>
+      </div>
+      <div className="flex flex-col">
+        <h1 className="text-2xl font-bold">Your Teams</h1>
+        {teams.map((team, index) => {
+          return (
+            <NavLink to={`/teams/${team.id}`} key={index}>
+              <div className="flex flex-col">
+                <h1 className="text-2xl font-bold">{team.name}</h1>
+                <p>{team.jobField}</p>
+                <p>{team.description}</p>
+              </div>
+            </NavLink>
+          );
+        })}
+      </div>
     </div>
   );
 };

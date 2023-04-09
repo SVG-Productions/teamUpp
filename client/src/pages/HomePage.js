e7c0a21b6e67c4520c7f852136c23ef22d12bdab
Gino Ochoa <gochoa1088@gmail.com>
2023-04-09 11:10:57 -0700

    re add authedUser check to protected route

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,6 +1,14 @@
 import Dashboard from "../components/Dashboard";
+import { useAuth } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
 
 const HomePage = () => {
+  const { authedUser } = useAuth();
+
+  if (!authedUser) {
+    return <Navigate to="/login" />;
+  }
+
   return <Dashboard />;
 };
 

29be727fa62a6f95eddc6563e7deffafdeaa01e2
Gino Ochoa <gochoa1088@gmail.com>
2023-04-10 11:36:29 -0700

    implement UnauthedLayout

--- client/src/components/UnauthedLayout.js
+++ client/src/components/UnauthedLayout.js
@@ -1,5 +1,18 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import Footer from "./Footer";
+
 const UnauthedLayout = () => {
-  return <div></div>;
+  const { authedUser } = useAuth();
+
+  return !authedUser ? (
+    <div className="flex flex-col justify-center items-center min-h-[calc(100vh-4rem)]">
+      <Outlet />
+      <Footer />
+    </div>
+  ) : (
+    <Navigate to="/" />
+  );
 };
 
 export default UnauthedLayout;

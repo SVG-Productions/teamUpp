813087d80bb563fd2ca4b64b18d83a34572a0e69
Gino Ochoa <gochoa1088@gmail.com>
2023-04-11 12:43:32 -0700

    remove error 404 handler

--- server/app.js
+++ server/app.js
@@ -81,9 +81,9 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function (err, req, res, next) {

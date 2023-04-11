8286b2226582ad1ce0acda5d56e8f2703d7caeb0
vincentochoa3 <vochoa893@gmail.com>
2023-04-11 14:49:22 -0700

    add css and components for teams page layout

--- client/src/components/ScrollableList.js
+++ client/src/components/ScrollableList.js
@@ -1,7 +1,7 @@
-const ScrollableList = ({ title, width, height = "auto", children }) => {
+const ScrollableList = ({ title, width, height = "sm:h-auto", children }) => {
   return (
     <div
-      className={`flex flex-col h-60 sm:h-auto sm:min-h-[200px] rounded-md w-auto ${width} ${height} bg-slate-100 shadow`}
+      className={`flex flex-col h-60 sm:min-h-[200px] rounded-md w-auto ${width} ${height} bg-slate-100 shadow`}
     >
       <div>
         <p className="relative z-10 p-3 font-bold shadow-[0_0.3px_1px_rgba(0,0,0,0.2)]">
--- client/src/pages/TeamsPage.js
+++ client/src/pages/TeamsPage.js
@@ -1,5 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { useLoaderData } from "react-router-dom";
+import ScrollableList from "../components/ScrollableList";
+import AuthedPageTitle from "../components/AuthedPageTitle";
 
 const TeamsPage = () => {
   const { allTeamsData, userTeamsData } = useLoaderData();
@@ -7,32 +9,33 @@ const TeamsPage = () => {
   const { userTeams } = userTeamsData.data;
 
   return (
-    <div className="flex flex-col">
-      <h1 className="text-2xl font-bold">All Teams</h1>
-      <div className="flex flex-col">
-        {teams.map((team, index) => (
-          <NavLink
-            to={`/teams/${team.id}`}
-            className="bg-white p-2.5 border-t-[0.5px] border-l-[0.5px] rounded-sm shadow-[0_0.3px_1px_rgba(0,0,0,0.2)] hover:bg-blue-200"
-            key={`${team.name}-${index}`}
-          >
-            {team.name}
-          </NavLink>
-        ))}
+    <>
+      <AuthedPageTitle>Teams</AuthedPageTitle>
+      <div className="flex sm:flex-row flex-col w-full gap-10 h-full mt-8 overflow-hidden">
+        <ScrollableList title="Teams" width="sm:w-3/4" height="sm:h-full">
+          {teams.map((team, index) => (
+            <NavLink
+              to={`/teams/${team.id}`}
+              className="bg-white p-2.5 border-t-[0.5px] border-l-[0.5px] rounded-sm shadow-[0_0.3px_1px_rgba(0,0,0,0.2)] hover:bg-blue-200"
+              key={`${team.name}-${index}`}
+            >
+              {team.name}
+            </NavLink>
+          ))}
+        </ScrollableList>
+        <ScrollableList title="Your Teams" width="sm:w-1/4" height="sm:h-full">
+          {userTeams.map((team) => (
+            <NavLink
+              to={`/teams/${team.id}`}
+              key={team.id}
+              className="bg-white p-2.5 border-t-[0.5px] border-l-[0.5px] rounded-sm shadow-[0_0.3px_1px_rgba(0,0,0,0.2)] hover:bg-blue-200"
+            >
+              {team.name}
+            </NavLink>
+          ))}
+        </ScrollableList>
       </div>
-      <div className="flex flex-col">
-        <h1 className="text-2xl font-bold">Your Teams</h1>
-        {userTeams.map((team) => (
-          <NavLink
-            to={`/teams/${team.id}`}
-            key={team.id}
-            className="bg-white p-2.5 border-t-[0.5px] border-l-[0.5px] rounded-sm shadow-[0_0.3px_1px_rgba(0,0,0,0.2)] hover:bg-blue-200"
-          >
-            {team.name}
-          </NavLink>
-        ))}
-      </div>
-    </div>
+    </>
   );
 };
 

720c84c756da6a81e471d7bf9fa3219ea8b1bc45
Gino Ochoa <gochoa1088@gmail.com>
2023-04-11 14:41:36 -0700

    adjustment to server

--- server/app.js
+++ server/app.js
@@ -56,18 +56,18 @@ if (process.env.NODE_ENV === "production") {
   app.get("/", (req, res) => {
     console.log("root route");
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.sendFile(path.join(__dirname, "build", "index.html"));
+    return res.sendFile(path.join(__dirname, "client", "build", "index.html"));
     return res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 
   // Serve the static assets in the frontend's build folder
-  // app.use(express.static(path.resolve("build")));
+  app.use(express.static(path.join(__dirname, "client", "build")));
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   app.get(/^(?!\/?api).*/, (req, res) => {
     console.log("not starting with api");
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.sendFile(path.join(__dirname, "build", "index.html"));
+    return res.sendFile(path.join(__dirname, "client", "build", "index.html"));
     return res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }

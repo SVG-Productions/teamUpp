caba37cd0160b160fa4daf5d546bf4d213cf8557
vincentochoa3 <vochoa893@gmail.com>
2023-04-12 11:18:01 -0700

    add useOnClickOutside hook, use on dropdowns and remove duplicate users_teams seeder data

--- client/src/components/SortByDropdown.js
+++ client/src/components/SortByDropdown.js
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import useOnClickOutside from "../hooks/useOnClickOutside";
 
 const SortByDropdown = ({ sortBy, setSortBy }) => {
   const [isListShowing, setIsListShowing] = useState(false);
+  const ref = useRef();
+  useOnClickOutside(ref, () => setIsListShowing(false));
 
   const handleClick = (value) => {
     setIsListShowing(false);
@@ -23,22 +26,25 @@ const SortByDropdown = ({ sortBy, setSortBy }) => {
         </button>
         <div className="relative">
           {isListShowing && (
-            <div className="absolute right-0 flex flex-col bg-slate-50 shadow-md text-center border border-t-0 rounded-sm">
+            <div
+              ref={ref}
+              className="absolute right-0 flex flex-col bg-slate-50 shadow-md text-center border border-t-0 rounded-sm"
+            >
               <button
                 onClick={() => handleClick("none")}
-                className="hover:bg-blue-200 w-16 text-sm"
+                className="hover:bg-blue-200 w-16 text-sm py-1"
               >
                 none
               </button>
               <button
                 onClick={() => handleClick("name")}
-                className="hover:bg-blue-200 w-16 text-sm"
+                className="hover:bg-blue-200 w-16 text-sm py-1"
               >
                 name
               </button>
               <button
                 onClick={() => handleClick("field")}
-                className="hover:bg-blue-200 w-16 text-sm"
+                className="hover:bg-blue-200 w-16 text-sm py-1"
               >
                 field
               </button>

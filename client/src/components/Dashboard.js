2c439fe2948610f62e49a35fd5623b31937730cb
Gino Ochoa <gochoa1088@gmail.com>
2023-04-15 22:23:58 -0700

    accept/deny invites in notifications of dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,4 +1,5 @@
-import { NavLink, useLoaderData } from "react-router-dom";
+import { NavLink, useLoaderData, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 import { useAuth } from "../context/AuthContext";
 import ScrollableList from "./ScrollableList";
@@ -7,112 +8,128 @@ import FavoriteButton from "./FavoriteButton";
 import DropdownMenuButton from "./DropdownMenuButton";
 import formatDate from "../utils/formatDate";
 
+const jobListings = [
+  {
+    company: "Acme Inc",
+    title: "Full Stack Engineer",
+    date: "2023-04-03",
+  },
+  {
+    company: "TechCorp",
+    title: "Senior Software Engineer",
+    date: "2023-04-02",
+  },
+  {
+    company: "InnovateX",
+    title: "Backend Developer",
+    date: "2023-04-01",
+  },
+  {
+    company: "BlueSky Co",
+    title: "Frontend Developer",
+    date: "2023-03-31",
+  },
+  {
+    company: "BigData Corp",
+    title: "Data Engineer",
+    date: "2023-03-30",
+  },
+  {
+    company: "CodeCloud",
+    title: "Cloud Solutions Architect",
+    date: "2023-03-29",
+  },
+  {
+    company: "Rocket Software",
+    title: "DevOps Engineer",
+    date: "2023-03-28",
+  },
+  {
+    company: "ByteCorp",
+    title: "Software Developer",
+    date: "2023-03-27",
+  },
+  {
+    company: "GlobalTech",
+    title: "Mobile Developer",
+    date: "2023-03-26",
+  },
+  {
+    company: "Code4U",
+    title: "UI/UX Designer",
+    date: "2023-03-25",
+  },
+  {
+    company: "CloudBuilders",
+    title: "Cloud Infrastructure Engineer",
+    date: "2023-03-24",
+  },
+  {
+    company: "RedDev",
+    title: "Software Engineer",
+    date: "2023-03-23",
+  },
+  {
+    company: "SaaS Inc",
+    title: "Senior Java Developer",
+    date: "2023-03-22",
+  },
+  {
+    company: "AI Technologies",
+    title: "Machine Learning Engineer",
+    date: "2023-03-21",
+  },
+  {
+    company: "GreenTech",
+    title: "Frontend Web Developer",
+    date: "2023-03-20",
+  },
+  {
+    company: "DataWorks",
+    title: "Data Analyst",
+    date: "2023-03-19",
+  },
+  {
+    company: "AgileSoft",
+    title: "Agile Coach",
+    date: "2023-03-18",
+  },
+  {
+    company: "SmartSolutions",
+    title: "Technical Lead",
+    date: "2023-03-17",
+  },
+  {
+    company: "SecureSoft",
+    title: "Security Engineer",
+    date: "2023-03-16",
+  },
+  {
+    company: "CodeX",
+    title: "Full Stack Developer",
+    date: "2023-03-15",
+  },
+];
+
 const Dashboard = () => {
   const { userTeams, invites } = useLoaderData();
   const { authedUser } = useAuth();
+  const navigate = useNavigate();
 
-  const jobListings = [
-    {
-      company: "Acme Inc",
-      title: "Full Stack Engineer",
-      date: "2023-04-03",
-    },
-    {
-      company: "TechCorp",
-      title: "Senior Software Engineer",
-      date: "2023-04-02",
-    },
-    {
-      company: "InnovateX",
-      title: "Backend Developer",
-      date: "2023-04-01",
-    },
-    {
-      company: "BlueSky Co",
-      title: "Frontend Developer",
-      date: "2023-03-31",
-    },
-    {
-      company: "BigData Corp",
-      title: "Data Engineer",
-      date: "2023-03-30",
-    },
-    {
-      company: "CodeCloud",
-      title: "Cloud Solutions Architect",
-      date: "2023-03-29",
-    },
-    {
-      company: "Rocket Software",
-      title: "DevOps Engineer",
-      date: "2023-03-28",
-    },
-    {
-      company: "ByteCorp",
-      title: "Software Developer",
-      date: "2023-03-27",
-    },
-    {
-      company: "GlobalTech",
-      title: "Mobile Developer",
-      date: "2023-03-26",
-    },
-    {
-      company: "Code4U",
-      title: "UI/UX Designer",
-      date: "2023-03-25",
-    },
-    {
-      company: "CloudBuilders",
-      title: "Cloud Infrastructure Engineer",
-      date: "2023-03-24",
-    },
-    {
-      company: "RedDev",
-      title: "Software Engineer",
-      date: "2023-03-23",
-    },
-    {
-      company: "SaaS Inc",
-      title: "Senior Java Developer",
-      date: "2023-03-22",
-    },
-    {
-      company: "AI Technologies",
-      title: "Machine Learning Engineer",
-      date: "2023-03-21",
-    },
-    {
-      company: "GreenTech",
-      title: "Frontend Web Developer",
-      date: "2023-03-20",
-    },
-    {
-      company: "DataWorks",
-      title: "Data Analyst",
-      date: "2023-03-19",
-    },
-    {
-      company: "AgileSoft",
-      title: "Agile Coach",
-      date: "2023-03-18",
-    },
-    {
-      company: "SmartSolutions",
-      title: "Technical Lead",
-      date: "2023-03-17",
-    },
-    {
-      company: "SecureSoft",
-      title: "Security Engineer",
-      date: "2023-03-16",
-    },
-    {
-      company: "CodeX",
-      title: "Full Stack Developer",
-      date: "2023-03-15",
-    },
-  ];
+  const handleAcceptInvite = async (team) => {
+    await axios.patch(`/api/teams/${team.id}/teammates`, {
+      userId: authedUser.id,
+      status: "member",
+    });
+    navigate(0);
+  };
+
+  const handleDenyInvite = async (team) => {
+    await axios.delete(`/api/teams/${team.id}/teammates`, {
+      data: { userId: authedUser.id },
+    });
+    navigate(0);
+  };
 
   return (
     <>
@@ -171,10 +188,24 @@ const Dashboard = () => {
           {invites.length ? (
             invites.map((team, index) => (
               <li
-                className="bg-white p-2.5 rounded-md"
+                className="flex bg-white p-2.5 rounded-md justify-between"
                 key={`${team.name}-${index}`}
               >
-                Invite to join {team.name}!
+                <span>Invite to join {team.name}!</span>
+                <div className="flex w-1/3 justify-around">
+                  <button
+                    className="py-1 px-2 w-2/5 bg-green-400 hover:bg-green-300 rounded-sm text-white text-sm"
+                    onClick={() => handleAcceptInvite(team)}
+                  >
+                    Accept
+                  </button>
+                  <button
+                    className="py-1 px-2 w-2/5 bg-red-400 hover:bg-red-300 rounded-sm text-white text-sm"
+                    onClick={() => handleDenyInvite(team)}
+                  >
+                    Deny
+                  </button>
+                </div>
               </li>
             ))
           ) : (

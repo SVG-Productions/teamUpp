03d49b534cf9aae16c5c21dba5dbed60df57f32e
vincentochoa3 <vochoa893@gmail.com>
2023-04-17 14:25:07 -0700

    add ui for buttons

--- client/src/pages/CreateListingPage.js
+++ client/src/pages/CreateListingPage.js
@@ -1,12 +1,19 @@
+import { NavLink } from "react-router-dom";
 import AuthedPageTitle from "../components/AuthedPageTitle";
 import FormField from "../components/FormField";
 
 export const CreateListingPage = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <>
       <AuthedPageTitle>Teams / Team Name / Create Listing</AuthedPageTitle>
       <div className="flex justify-center">
-        <form className="max-w-4xl w-full mt-8 p-6 bg-slate-100 border shadow">
+        <form
+          onSubmit={handleSubmit}
+          className="max-w-4xl w-full mt-8 p-6 bg-slate-100 border shadow"
+        >
           <div className="sm:w-2/3">
             <FormField
               label="Job Title"
@@ -48,6 +55,14 @@ export const CreateListingPage = () => {
               className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-slate-400 resize-none"
               required={false}
             />
+          </div>
+          <div className="flex justify-center align-center gap-5 mt-5">
+            <NavLink className="w-1/4 min-w-[84px] text-sm sm:text-base text-center border-2 bg-white border-slate-600 hover:bg-red-200 text-slate-600 font-bold py-2 px-4 rounded focus:shadow-outline">
+              Cancel
+            </NavLink>
+            <button className="w-1/4 min-w-[84px] text-sm sm:text-base border-2 bg-white border-slate-600 hover:bg-blue-200 text-slate-600 font-bold py-2 px-4 rounded focus:shadow-outline">
+              Create
+            </button>
           </div>
         </form>
       </div>

a9712b325b43fdc06d997b795f0f3a0c2001f75f
Gino Ochoa <gochoa1088@gmail.com>
2023-04-18 11:24:38 -0700

    add loading check to authed layout

--- client/src/components/AuthedLayout.js
+++ client/src/components/AuthedLayout.js
@@ -1,4 +1,4 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useNavigation } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import AuthedPageContainer from "./AuthedPageContainer";
@@ -6,15 +6,25 @@ import { useAuth } from "../context/AuthContext";
 
 const AuthedLayout = ({ children }) => {
   const { authedUser } = useAuth();
+  const navigation = useNavigation();
 
-  return authedUser ? (
-    <div className="flex flex-col min-h-screen items-center bg-white">
-      <Navbar />
-      <AuthedPageContainer>{children || <Outlet />}</AuthedPageContainer>
-      <Footer />
-    </div>
-  ) : (
-    <Navigate to="/login" />
+  if (!authedUser) {
+    return <Navigate to="/login" />;
+  }
+
+  return (
+    <>
+      {navigation.state === "loading" && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
+          <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-gray-900"></div>
+        </div>
+      )}
+      <div className="flex flex-col min-h-screen items-center bg-white">
+        <Navbar />
+        <AuthedPageContainer>{children || <Outlet />}</AuthedPageContainer>
+        <Footer />
+      </div>
+    </>
   );
 };
 

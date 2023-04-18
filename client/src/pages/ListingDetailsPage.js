a75970ad0b3bbacaf6f3238dc8b32b4c8248449a
Gino Ochoa <gochoa1088@gmail.com>
2023-04-17 18:57:25 -0700

    page title

--- client/src/pages/ListingDetailsPage.js
+++ client/src/pages/ListingDetailsPage.js
@@ -1,12 +1,29 @@
-import { useLoaderData } from "react-router-dom";
+import axios from "axios";
+import { NavLink, useLoaderData } from "react-router-dom";
+import AuthedPageTitle from "../components/AuthedPageTitle";
 
 export const ListingDetailsPage = () => {
-  const { team, teammates, listin } = useLoaderData();
-  return <div>LISTING DETAILS PAGE</div>;
+  const { team, teammates, listing } = useLoaderData();
+  console.log(team, teammates, listing);
+  return (
+    <>
+      <AuthedPageTitle>
+        <NavLink to={`/teams/${team.id}`} className="hover:underline">
+          {team.name}
+        </NavLink>{" "}
+        /{" "}
+        <NavLink to={`/teams/${team.id}`} className="hover:underline">
+          Listings
+        </NavLink>{" "}
+        / {listing.companyName} - {listing.jobTitle}
+      </AuthedPageTitle>
+      <div className="flex flex-col sm:flex-row gap-10 mt-8 w-full h-[90%]"></div>
+    </>
+  );
 };
 
 export const listingDetailsLoader = async ({ request, params }) => {
-  const { teamId, listingId } = req.params;
+  const { teamId, listingId } = params;
   const [teamData, teammatesData, listingData] = await Promise.all([
     axios.get(`/api/teams/${teamId}`),
     axios.get(`/api/teams/${teamId}/teammates`),
@@ -14,7 +31,9 @@ export const listingDetailsLoader = async ({ request, params }) => {
   ]);
 
   const team = teamData.data;
-  const teammates = teammatesData.data;
+  const teammates = teammatesData.data.filter(
+    (tm) => tm.status !== "invited" && tm.status !== "requested"
+  );
   const listing = listingData.data;
 
   return { team, teammates, listing };

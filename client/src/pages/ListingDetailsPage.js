7d9fe7a73669c917f06e8951682b1b191272da2a
Gino Ochoa <gochoa1088@gmail.com>
2023-04-17 19:33:54 -0700

    implement tabs

--- client/src/pages/ListingDetailsPage.js
+++ client/src/pages/ListingDetailsPage.js
@@ -20,17 +20,37 @@ export const ListingDetailsPage = () => {
       </AuthedPageTitle>
       <div className="flex flex-col gap-10 mt-8 w-full h-[90%]">
         <div className="flex flex-col h-2/3 w-full">
-          <div className="flex gap-2 w-1/4">
-            <NavLink>Details</NavLink>
-            <NavLink>Experiences</NavLink>
+          <div className="flex gap-3 w-1/4 px-2">
+            <NavLink
+              className={({ isActive }) =>
+                `border-black pb-1 w-28 text-center ${
+                  isActive ? "border-b-4 font-bold" : "border-b"
+                }`
+              }
+              to={`/teams/${team.id}/listings/${listing.id}/details`}
+            >
+              Details
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                `border-black pb-1 w-28 text-center ${
+                  isActive ? "border-b-4 font-bold" : "border-b"
+                }`
+              }
+              to={`/teams/${team.id}/listings/${listing.id}/experiences`}
+            >
+              Experiences
+            </NavLink>
           </div>
-          <div className="flex h-full">
+          <div className="flex flex-col sm:flex-row h-full">
             <div className="h-full w-1/2">Details</div>
             <div className="h-full w-1/2">Description</div>
           </div>
         </div>
-        <div className="flex gap-6 h-1/3">
-          <ScrollableList title="Comments" width="sm:w-3/5"></ScrollableList>
+        <div className="flex flex-col sm:flex-row gap-6 h-1/3">
+          <ScrollableList title="Comments" width="sm:w-3/5">
+            {/* TODO: Comments UI and backend work */}
+          </ScrollableList>
           <ScrollableList title="All Teammates" width="sm:w-2/5">
             {teammates.map((teammate, index) => (
               <NavLink

bd24c0f048e9377525f1f6cac1d5bed51d00bbf0
Gino Ochoa <gochoa1088@gmail.com>
2023-04-20 11:49:35 -0700

    add message to login loading

--- client/src/components/UnauthedLayout.js
+++ client/src/components/UnauthedLayout.js
@@ -1,4 +1,4 @@
-import { Navigate, Outlet, useNavigation } from "react-router-dom";
+import { Navigate, Outlet, useLocation, useNavigation } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import Footer from "./Footer";
 import LoadingSpinner from "./LoadingSpinner";
@@ -6,11 +6,20 @@ import LoadingSpinner from "./LoadingSpinner";
 const UnauthedLayout = ({ children }) => {
   const { authedUser } = useAuth();
   const navigation = useNavigation();
+  console.log(navigation);
+  const location = useLocation();
+  console.log(location);
 
   if (navigation.state === "loading") {
     return (
       <>
-        <LoadingSpinner />
+        <LoadingSpinner
+          message={
+            location.pathname === "/login"
+              ? "Logging in..."
+              : "Creating user..."
+          }
+        />
         <div className="flex flex-col justify-center items-center min-h-[calc(100vh-4rem)]">
           {children || <Outlet />}
         </div>

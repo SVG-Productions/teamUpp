ff22d633a03c13d34179543e4a9ee7a9a2eb849b
Gino Ochoa <gochoa1088@gmail.com>
2023-04-19 17:34:46 -0700

    comments for clarity and removal of jobfield criteria

--- server/models/Team.js
+++ server/models/Team.js
@@ -80,49 +80,44 @@ const getAllTeamListings = async (teamId) => {
 const getRecommendedTeams = async (userId) => {
   try {
     // Retrieve the list of teams that the user is currently a member of
-    const userTeams = await knex("users_teams")
-      .where("user_id", userId)
-      .join("teams", "users_teams.team_id", "=", "teams.id")
-      .select("teams.id", "teams.name", "teams.job_field", "teams.description");
-
-    // Retrieve the list of users who are members of those teams, excluding the caller
-    const teamUsers = await knex("users_teams")
-      .whereIn(
-        "team_id",
-        userTeams.map((team) => team.id)
-      )
-      .join("users", "users_teams.user_id", "=", "users.id")
-      .whereNot("users.id", userId)
-      .select("users.id");
+    const userTeams = (
+      await knex("users_teams")
+        .where("user_id", userId)
+        .join("teams", "users_teams.team_id", "=", "teams.id")
+        .select("teams.id")
+    ).map((team) => team.id);
 
     // Retrieve the list of teams that those remaining users are members of
-    const otherUserTeams = await knex("users_teams")
-      .whereIn(
-        "user_id",
-        teamUsers.map((user) => user.id)
-      )
+    const recommendedTeams = await knex("users_teams")
       .join("teams", "users_teams.team_id", "=", "teams.id")
       .select("teams.id", "teams.name", "teams.job_field", "teams.description")
+      .whereIn(
+        // includes teams that are asssociated with all of user's teammates
+        "user_id",
+        knex("users_teams") // list of userIds that matches the following criteria ------------------------------}
+          .join("users", "users_teams.user_id", "=", "users.id") //                                              }
+          .select("users.id") //                                                                                 }
+          .whereIn("team_id", userTeams) // only include userIds which are associated with the user's teams      }
+      ) // ------------------------------------------------------------------------------------------------------}
+      .whereNotIn("team_id", userTeams) // but excludes teams the user is already in
       .distinct();
 
-    // Filter out any teams that the current user is already a member of
-    const recommendedTeams = otherUserTeams.filter(
-      (team) => !userTeams.find((userTeam) => userTeam.id === team.id)
-    );
-
     // Retrieve the list of teams that have the same job field as the current user
-    const userJobField = userTeams[0]?.job_field;
-    if (userJobField) {
-      const jobFieldTeams = await knex("teams")
-        .where("job_field", userJobField)
-        .andWhereNot(
-          "id",
-          userTeams.map((team) => team.id)
-        )
-        .select("id", "name", "job_field", "description");
+    //Below needs to be done a different way, let user select a job field on account creation or profile settings?
 
-      recommendedTeams.push(...jobFieldTeams);
-    }
+    // const userJobField = userTeams[0]?.job_field;
+    // if (userJobField) {
+    //   const jobFieldTeams = await knex("teams")
+    //     .where("job_field", userJobField)
+    //     .andWhereNot(
+    //       "id",
+    //       userTeams.map((team) => team.id)
+    //     )
+    //     .select("id", "name", "job_field", "description");
+
+    //   recommendedTeams.push(...jobFieldTeams);
+    // }
+    // console.log(recommendedTeams, recommendedTeams.length);
 
     return recommendedTeams;
   } catch (error) {

c232b97fc796f6d459bcd54fad2a0dcf11edfb36
vincentochoa3 <vochoa893@gmail.com>
2023-04-21 10:41:09 -0700

    change ScrollableList add button color

--- client/src/components/ScrollableList.js
+++ client/src/components/ScrollableList.js
@@ -25,7 +25,7 @@ const ScrollableList = ({
           {hasAddButton && (
             <button
               onClick={onClick}
-              className="flex items-center justify-center w-6 h-6 rounded-full bg-emerald-400 hover:bg-emerald-600 text-white font-bold"
+              className="flex items-center justify-center w-6 h-6 rounded-full bg-slate-900 hover:bg-slate-400 text-white font-bold"
             >
               +
             </button>

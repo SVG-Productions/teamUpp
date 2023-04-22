ccdd421d3150da868f40929285ff14827a2193e0
vincentochoa3 <vochoa893@gmail.com>
2023-04-21 18:06:45 -0700

    add AcceptButton and DenyButton files

---
+++ client/src/components/AcceptButton.js
@@ -0,0 +1,21 @@
+const AcceptButton = () => {
+  return (
+    <button>
+      <svg
+        clip-rule="evenodd"
+        fill-rule="evenodd"
+        stroke-linejoin="round"
+        stroke-miterlimit="2"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="m21 4.009c0-.478-.379-1-1-1h-16c-.62 0-1 .519-1 1v16c0 .621.52 1 1 1h16c.478 0 1-.379 1-1zm-16.5.5h15v15h-15zm2.449 7.882 3.851 3.43c.142.128.321.19.499.19.202 0 .405-.081.552-.242l5.953-6.509c.131-.143.196-.323.196-.502 0-.41-.331-.747-.748-.747-.204 0-.405.082-.554.243l-5.453 5.962-3.298-2.938c-.144-.127-.321-.19-.499-.19-.415 0-.748.335-.748.746 0 .205.084.409.249.557z"
+          fill-rule="nonzero"
+        />
+      </svg>
+    </button>
+  );
+};
+
+export default AcceptButton;

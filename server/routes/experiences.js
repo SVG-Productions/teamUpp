e1efe6c09a7a0eb9ebf1fe24b0c1b0934cd665df
Schaeffer <schaefferahn@gmail.com>
2023-04-24 14:57:02 -0700

    controllers, models, routes for get single experience

--- server/routes/experiences.js
+++ server/routes/experiences.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const { getSingleExperience } = require("../controllers/experiencesController");
+
 /* GET experiences. */
 router.get("/", function (req, res, next) {
   res.json({ users: ["experienceOne", "experienceTwo", "experienceThree"] });
 });
+router.get("/:experienceId", getSingleExperience);
 
 module.exports = router;

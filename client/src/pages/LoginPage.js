a01b189cc25dc3f975f6dde3f4eae33aaf173fb0
Gino Ochoa <gochoa1088@gmail.com>
2023-04-25 13:33:29 -0700

    delete import from login

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -5,7 +5,6 @@ import FormField from "../components/FormField";
 import AuthFormButton from "../components/AuthFormButton";
 import AuthFormRedirect from "../components/AuthFormRedirect";
 import Logo from "../components/Logo";
-import DeleteExperienceModal from "../components/DeleteExperienceModal";
 
 export const LoginPage = () => {
   const [credential, setCredential] = useState("");

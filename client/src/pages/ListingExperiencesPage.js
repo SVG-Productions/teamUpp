2f5ce2bf44e026912fea592b287efcc2224a2048
Gino Ochoa <gochoa1088@gmail.com>
2023-04-24 23:05:45 -0700

    implement experiences loader

--- client/src/pages/ListingExperiencesPage.js
+++ client/src/pages/ListingExperiencesPage.js
@@ -1,3 +1,24 @@
+import axios from "axios";
+
 export const ListingExperiencesPage = () => {
   return <div>LISTING EXPERIENCES PAGE</div>;
+};
+
+export const listingExperiencesLoader = async ({ request, params }) => {
+  const { teamId, listingId } = params;
+
+  const [teamResponse, listingResponse, userResponse] = await Promise.all([
+    axios.get(`/api/teams/${teamId}`),
+    axios.get(`/api/listings/${listingId}`),
+    axios.get("/api/session/user"),
+  ]);
+
+  const { team, teammates } = teamResponse.data;
+  const filteredTeammates = teammates.filter(
+    (tm) => tm.status !== "invited" && tm.status !== "requested"
+  );
+  const { listing, comments } = listingResponse.data;
+  const { favorites } = userResponse.data;
+
+  return { team, teammates: filteredTeammates, listing, favorites, comments };
 };

9ce09250fa33f6c7b0ef44e554e503e5e7d96ba6
Gino Ochoa <gochoa1088@gmail.com>
2023-04-25 12:29:45 -0700

    css for modal

--- client/src/components/DeleteExperienceModal.js
+++ client/src/components/DeleteExperienceModal.js
@@ -1,7 +1,46 @@
 import { useState } from "react";
 
 const DeleteExperienceModal = () => {
-  return <div></div>;
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  return (
+    <div
+      className={`fixed inset-0 z-50 ${isOpen ? "" : "hidden"} overflow-y-auto`}
+    >
+      <div className="flex items-center justify-center min-h-screen">
+        <div
+          className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+          onClick={onClose}
+        ></div>
+
+        <div className="relative bg-white w-full max-w-md mx-auto rounded-lg shadow-lg z-10">
+          <div className="p-4">
+            <h2 className="text-lg font-medium mb-4">
+              Are you sure you want to delete this post?
+            </h2>
+
+            <div className="flex justify-end">
+              <button
+                className="bg-gray-100 text-gray-700 font-medium px-4 py-2 mr-2 rounded-lg"
+                onClick={onClose}
+                disabled={isDeleting}
+              >
+                Cancel
+              </button>
+
+              <button
+                className="bg-red-500 text-white font-medium px-4 py-2 rounded-lg"
+                onClick={handleDelete}
+                disabled={isDeleting}
+              >
+                {isDeleting ? "Deleting..." : "Delete"}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DeleteExperienceModal;

36c6671e7805be6dd3ccac8eb739abd249449eac
Gino Ochoa <gochoa1088@gmail.com>
2023-04-25 17:51:03 -0700

    create ListingTeammatesSection

---
+++ client/src/components/ListingTeammatesSection.js
@@ -0,0 +1,20 @@
+import ScrollableList from "./ScrollableList";
+
+const ListingTeammatesSection = (teammates) => {
+  return (
+    <ScrollableList title="All Teammates" width="sm:w-2/5">
+      {teammates.map((teammate, index) => (
+        <NavLink
+          key={`${teammate.id}-${index}`}
+          to={`/${teammate.username}`}
+          className="flex bg-slate-100 p-2.5 rounded-sm hover:bg-blue-100"
+        >
+          <div className="bg-white rounded-full w-6 h-6 mr-4" />
+          <p> {teammate.username}</p>
+        </NavLink>
+      ))}
+    </ScrollableList>
+  );
+};
+
+export default ListingTeammatesSection;

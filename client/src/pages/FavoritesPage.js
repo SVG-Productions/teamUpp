13673faa29fbe3fc7df992c56a7696cca2b2d1db
vincentochoa3 <vochoa893@gmail.com>
2023-04-26 14:13:23 -0700

    adjust FavoritesPage listings size

--- client/src/pages/FavoritesPage.js
+++ client/src/pages/FavoritesPage.js
@@ -20,35 +20,38 @@ export const FavoritesPage = () => {
         </NavLink>{" "}
         / Favorites
       </AuthedPageTitle>
-      <ScrollableList
-        title="Favorite Listings"
-        width="w-full min-w-[325px] -mx-4 sm:mx-0"
-        height="h-[40rem] sm:h-[100%] min-h-[430px] mt-8"
-      >
-        {favorites.map((listing, index) => (
-          <div key={index} className="flex flex-row bg-white p-2.5 rounded-md">
-            <div className="flex flex-row w-2/3 items-center">
-              <FavoriteButton listing={listing} />
-              <div className="text-xs sm:text-lg font-bold">
-                {listing.companyName}
+      <div className="mt-8 h-full">
+        <ScrollableList title="Favorite Listings">
+          {favorites.map((listing, index) => (
+            <div
+              key={index}
+              className="flex flex-row bg-white p-2.5 rounded-md"
+            >
+              <div className="flex flex-row w-2/3 items-center">
+                <FavoriteButton listing={listing} />
+                <div className="text-xs sm:text-lg font-bold">
+                  {listing.companyName}
+                </div>
+                <div className="hidden sm:block sm:text-lg font-bold mx-2">
+                  /
+                </div>
+                <NavLink
+                  className="text-xs sm:text-base px-3 hover:underline sm:px-0"
+                  to={`/teams/${listing.teamId}/listings/${listing.id}/details`}
+                >
+                  {listing.jobTitle}
+                </NavLink>
               </div>
-              <div className="hidden sm:block sm:text-lg font-bold mx-2">/</div>
-              <NavLink
-                className="text-xs sm:text-base px-3 hover:underline sm:px-0"
-                to={`/teams/${listing.teamId}/listings/${listing.id}/details`}
-              >
-                {listing.jobTitle}
-              </NavLink>
-            </div>
-            <div className="flex flex-row justify-end w-1/3 items-center">
-              <div className="text-xs sm:text-sm">
-                {formatDate(listing.createdAt)}
+              <div className="flex flex-row justify-end w-1/3 items-center">
+                <div className="text-xs sm:text-sm">
+                  {formatDate(listing.createdAt)}
+                </div>
+                <DropdownMenuButton />
               </div>
-              <DropdownMenuButton />
             </div>
-          </div>
-        ))}
-      </ScrollableList>
+          ))}
+        </ScrollableList>
+      </div>
     </>
   );
 };

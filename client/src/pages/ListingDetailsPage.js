c5693d7aa25f9160af93b2ce7869108c2145d7de
Gino Ochoa <gochoa1088@gmail.com>
2023-04-26 17:02:36 -0700

    create ListingDetails component

--- client/src/pages/ListingDetailsPage.js
+++ client/src/pages/ListingDetailsPage.js
@@ -9,7 +9,7 @@ import ListingTeammatesSection from "../components/ListingTeammatesSection";
 import ListingTabs from "../components/ListingTabs";
 
 export const ListingDetailsPage = () => {
-  const { team, teammates, listing } = useLoaderData();
+  const { team, listing } = useLoaderData();
   const { authedUser } = useAuth();
 
   return (
@@ -36,46 +36,6 @@ export const ListingDetailsPage = () => {
       <div className="flex flex-col gap-10 mt-8 w-full h-[90%]">
         <div className="flex flex-col min-h-3/5 w-full">
           <ListingTabs />
-          <div
-            className="flex flex-col sm:flex-row pt-1 sm:min-h-[350px] sm:max-h-[350px]
-          rounded-md w-auto bg-slate-100 shadow"
-          >
-            <div
-              className="flex flex-col gap-4 h-full w-full sm:w-1/2 sm:border-8
-              sm:border-transparent sm:border-r-2 sm:border-r-black p-4 overflow-y-scroll"
-            >
-              <div>
-                <p className="font-bold">Job Title:</p>
-                <p>{listing.jobTitle}</p>
-              </div>
-              <div>
-                <p className="font-bold">Link to Apply:</p>
-                <a
-                  className="hover:underline"
-                  target="_blank"
-                  rel="noreferrer"
-                  href={`${listing.jobLink}`}
-                >
-                  {listing.jobLink}
-                </a>
-              </div>
-              <div>
-                <p className="font-bold">Company Name:</p>
-                <p>{listing.companyName}</p>
-              </div>
-              <div>
-                <p className="font-bold">Company Details:</p>
-                <p>{listing.companyDetails}</p>
-              </div>
-            </div>
-            <div
-              className="h-full w-full sm:w-1/2 sm:border-8 sm:border-transparent
-            sm:border-l-2 sm:border-l-black  p-4"
-            >
-              <p className="font-bold">Job Description:</p>
-              <p>{listing.jobDescription}</p>
-            </div>
-          </div>
         </div>
         <div className="flex flex-col sm:flex-row gap-6 h-2/5">
           <div className="sm:w-3/5 sm:h-auto h-60">

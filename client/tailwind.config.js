8ac6cc722793df354c1eeeedd38a2582bb2d2037
Gino Ochoa <gochoa1088@gmail.com>
2023-05-06 16:40:57 -0700

    add tailwind colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      bluegray: "#4773AA ",
+      highlightblue: "#BFE0FF",
+    },
     extend: {},
   },
   plugins: [],

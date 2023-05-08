ac71a15c55389bbecc2166974a459518edbc39f1
Gino Ochoa <gochoa1088@gmail.com>
2023-05-08 16:43:01 -0700

    experiences_questions up function

--- server/migrations/20230508233442_experiences-questions.js
+++ server/migrations/20230508233442_experiences-questions.js
@@ -2,14 +2,22 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = async function (knex) {
+  await knex.schema.createTable("experiences_questions", function (table) {
+    table.uuid("id").defaultTo(knex.raw("gen_random_uuid()")).primary();
+    table
+      .uuid("experience_id")
+      .notNullable()
+      .references("id")
+      .inTable("experiences")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table.text("question").notNullable();
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {};

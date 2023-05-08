dedabd9b150c3241c92a18a921451e64c8cc8c54
vincentochoa3 <vochoa893@gmail.com>
2023-05-08 13:37:39 -0700

    hide copyright in footer when in mobile

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="flex w-full p-2 sm:p-4 h-16 border-t-[1px] items-center justify-between sm:text-center text-xs sm:text-sm bg-slate-100">
-      <div className="text-slate-300">
+    <div className="flex w-full p-2 sm:p-4 h-16 border-t-[1px] items-center justify-center sm:justify-between sm:text-center text-xs sm:text-sm bg-slate-100">
+      <div className="text-slate-300 hidden sm:block">
         Copyright &copy; 2023 SVG Productions
       </div>
       <div className="flex justify-center gap-1 sm:gap-3 text-center">

466446599c581f7608a5dc6671987457c62a5d20
Gino Ochoa <gochoa1088@gmail.com>
2023-05-08 22:29:53 -0700

    implement questionsController

--- server/routes/questions.js
+++ server/routes/questions.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  createExperienceQuestion,
-  updateExperienceQuestion,
-  deleteExperienceQuestion,
-} = require("../controllers/experiencesController");
+  addQuestion,
+  updateQuestion,
+  deleteQuestion,
+} = require("../controllers/questionsController");
 
-router.post("/", createExperienceQuestion);
-router.patch("/:questionId", updateExperienceQuestion);
-router.delete("/:questionId", deleteExperienceQuestion);
+router.post("/", addQuestion);
+router.patch("/:questionId", updateQuestion);
+router.delete("/:questionId", deleteQuestion);
 
 module.exports = router;

f2d386b7f1cfed44d0fa9add114d7f4b25741969
vincentochoa3 <vochoa893@gmail.com>
2023-05-10 11:18:55 -0700

    add collapsible UserIntersts for mobile query

--- client/src/components/UserInterests.js
+++ client/src/components/UserInterests.js
@@ -1,27 +1,58 @@
 import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
 import NullInfo from "./NullInfo";
 
 const UserInterests = () => {
   const { jobFields } = useLoaderData();
 
+  const [isUserInterestsShowing, setIsUserInterestsShowing] = useState(false);
+
   return (
-    <div>
-      <p className="font-bold text-slate-400">INTERESTS</p>
-      {!jobFields.length ? (
-        <NullInfo />
-      ) : (
-        <div className="flex flex-col lg:flex-row gap-3 p-2">
-          {jobFields.map((interest, i) => (
-            <div
-              key={`${interest}-${i}`}
-              className="bg-slate-100 py-1 px-2 rounded-full w-fit"
-            >
-              <p className="capitalize">{interest}</p>
-            </div>
-          ))}
+    <>
+      <div className="hidden sm:block">
+        <p className="font-bold text-slate-400">INTERESTS</p>
+        {!jobFields.length ? (
+          <NullInfo />
+        ) : (
+          <div className="flex flex-col lg:flex-row gap-3 p-2">
+            {jobFields.map((interest, i) => (
+              <div
+                key={`${interest}-${i}`}
+                className="bg-slate-100 py-1 px-2 rounded-full w-fit"
+              >
+                <p className="capitalize">{interest}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="sm:hidden">
+        <div
+          onClick={() => setIsUserInterestsShowing(!isUserInterestsShowing)}
+          className="flex justify-between cursor-pointer"
+        >
+          <p className="font-bold text-slate-400">INTERESTS</p>
+          {isUserInterestsShowing && jobFields.length ? (
+            <div className="text-slate-500">&#9650;</div>
+          ) : (
+            <div className="text-slate-500">&#9660;</div>
+          )}
         </div>
-      )}
-    </div>
+        {!jobFields.length && <NullInfo />}
+        {isUserInterestsShowing && (
+          <ul className="flex flex-col lg:flex-row gap-3 p-2">
+            {jobFields.map((interest, i) => (
+              <div
+                key={`${interest}-${i}`}
+                className="bg-slate-100 py-1 px-2 rounded-full w-fit"
+              >
+                <p className="capitalize">{interest}</p>
+              </div>
+            ))}
+          </ul>
+        )}
+      </div>
+    </>
   );
 };
 

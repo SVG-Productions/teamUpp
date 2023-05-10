5430234bc9a69874111f1affda160dc4fda248f7
Gino Ochoa <gochoa1088@gmail.com>
2023-05-10 12:48:59 -0700

    clean up toggle

--- client/src/pages/UserSettingsPage.js
+++ client/src/pages/UserSettingsPage.js
@@ -132,18 +132,10 @@ export const UserSettingsPage = () => {
                     type="checkbox"
                     defaultChecked={isEmailPublic}
                     onChange={() => setIsEmailPublic(!isEmailPublic)}
-                    className="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 
-                    before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full 
-                    before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] 
-                    after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 
-                    after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] 
-                    checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] 
-                    checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full 
-                    checked:after:border-none checked:after:bg-blue-400 
-                    checked:after:shadow-bluegraylight 
-                    checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] 
-                    hover:cursor-pointer  dark:bg-neutral-600 dark:after:bg-neutral-400 
-                   "
+                    className="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem]
+                    after:absolute after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full 
+                    after:transition-[background-color_0.2s,transform_0.2s] checked:after:ml-[1.0625rem] 
+                    checked:after:bg-blue-400 hover:cursor-pointer dark:bg-neutral-600 dark:after:bg-neutral-400"
                   />
                 </div>
               </div>

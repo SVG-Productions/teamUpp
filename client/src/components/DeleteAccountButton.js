7924c27cd17be70dccbfbd3024c931c5bc3338e8
Gino Ochoa <gochoa1088@gmail.com>
2023-05-10 18:57:01 -0700

    DeleteAccountModal

--- client/src/components/DeleteAccountButton.js
+++ client/src/components/DeleteAccountButton.js
@@ -1,16 +1,20 @@
-import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
+import DeleteAccountModal from "./DeleteAccountModal";
 
 const DeleteAccountButton = () => {
-  const { user } = useLoaderData();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <button
-      className="border-2 rounded h-[75%] justify-center self-center text-xs 
-  font-bold text-red-500 bg-white border-red-500 hover:bg-red-200 p-2 mt-1"
-      to={`/${user.username}/settings/delete-account`}
-    >
-      Delete Account
-    </button>
+    <>
+      <button
+        className="border-2 rounded h-[75%] justify-center self-center text-xs 
+      font-bold text-red-500 bg-white border-red-500 hover:bg-red-200 p-2 mt-1"
+        onClick={() => setIsModalOpen(true)}
+      >
+        Delete Account
+      </button>
+      {isModalOpen && <DeleteAccountModal handleModal={setIsModalOpen} />}
+    </>
   );
 };
 

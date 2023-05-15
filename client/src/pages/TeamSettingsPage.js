401795f4c8bcab472bf0a5a25d7fbca1c0100c33
vincentochoa3 <vochoa893@gmail.com>
2023-05-15 16:11:57 -0700

    add button positioning

--- client/src/pages/TeamSettingsPage.js
+++ client/src/pages/TeamSettingsPage.js
@@ -6,6 +6,7 @@ import FormField from "../components/FormField";
 import { useAuth } from "../context/AuthContext";
 import { jobFieldsData } from "../utils/jobFieldsData";
 import FormToggle from "../components/FormToggle";
+import PencilButton from "../components/PencilButton";
 
 export const TeamSettingsPage = () => {
   const { team, ownerId } = useLoaderData();
@@ -72,7 +73,7 @@ export const TeamSettingsPage = () => {
           className="relative max-w-4xl w-full mt-8 p-6"
         >
           <div className="flex">
-            <div className="flex flex-col">
+            <div className="flex flex-col w-1/2 gap-4">
               <FormField
                 label="TEAM NAME"
                 id="name"
@@ -83,7 +84,7 @@ export const TeamSettingsPage = () => {
               />
               <label
                 htmlFor="jobField"
-                className="block font-semibold text-slate-400 mb-2 text-sm"
+                className="block font-bold text-slate-400 mb-2 text-sm"
               >
                 JOB FIELD
               </label>
@@ -135,6 +136,19 @@ export const TeamSettingsPage = () => {
                 />
               </div>
             </div>
+            <div className="flex flex-col w-1/2 items-center">
+              <p className="block font-bold self-start text-slate-400 mb-4 text-sm sm:ml-16 sm:mb-2">
+                TEAM PICTURE
+              </p>
+              <div className="relative w-40 h-40 rounded-full bg-bluegraylight sm:w-56 sm:h-56 sm:mt-12">
+                <PencilButton
+                  href=""
+                  styling="absolute w-8 h-8 bottom-2 left-2 sm:bottom-4 sm:left-4 bg-slate-900"
+                  iconSize="16px"
+                  fill="white"
+                />
+              </div>
+            </div>
           </div>
           <div className="flex flex-col mt-3">
             <label
@@ -154,16 +168,20 @@ export const TeamSettingsPage = () => {
               required={false}
             />
           </div>
-          <div className="flex justify-center align-center gap-5 mt-5">
+          <div className="flex justify-end align-center gap-5 mt-5">
+            <button
+              className="w-1/4 min-w-[84px] text-sm bg-bluegray hover:bg-blue-900 text-white 
+              font-bold py-2 px-4 rounded-md focus:shadow-outline sm:w-1/6 sm:text-base"
+            >
+              Save
+            </button>
             <NavLink
               to={`/teams/${team.id}`}
-              className="w-1/4 min-w-[84px] text-sm sm:text-base text-center border bg-white border-slate-600 hover:bg-red-200 text-slate-600 font-bold py-2 px-4 rounded focus:shadow-outline"
+              className="w-1/4 min-w-[84px] text-sm text-center bg-white hover:bg-gray-300 border-2 
+              text-black font-bold py-2 px-4 rounded-md focus:shadow-outline sm:w-1/6 sm:text-base"
             >
               Cancel
             </NavLink>
-            <button className="w-1/4 min-w-[84px] text-sm sm:text-base border bg-white border-slate-600 hover:bg-blue-200 text-slate-600 font-bold py-2 px-4 rounded focus:shadow-outline">
-              Save
-            </button>
           </div>
         </form>
       </div>

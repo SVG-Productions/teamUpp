3f07a9b2b0afe57ca36d499720504d5d8771d3fe
vincentochoa3 <vochoa893@gmail.com>
2023-05-15 15:59:23 -0700

    add positioning for FormToggle and ensure styling maintained in UserProfileSettings

--- client/src/pages/TeamSettingsPage.js
+++ client/src/pages/TeamSettingsPage.js
@@ -5,6 +5,7 @@ import AuthedPageTitle from "../components/AuthedPageTitle";
 import FormField from "../components/FormField";
 import { useAuth } from "../context/AuthContext";
 import { jobFieldsData } from "../utils/jobFieldsData";
+import FormToggle from "../components/FormToggle";
 
 export const TeamSettingsPage = () => {
   const { team, ownerId } = useLoaderData();
@@ -68,92 +69,79 @@ export const TeamSettingsPage = () => {
       <div className="flex justify-center">
         <form
           onSubmit={handleSubmit}
-          className="relative max-w-4xl w-full mt-8 p-6 bg-slate-100 border shadow"
+          className="relative max-w-4xl w-full mt-8 p-6"
         >
-          <div className="flex flex-row">
-            <div className="sm:w-2/3">
+          <div className="flex">
+            <div className="flex flex-col">
               <FormField
-                label="Team Name"
+                label="TEAM NAME"
                 id="name"
                 type="text"
                 placeholder={name}
                 value={name}
                 onChange={(e) => setName(e.target.value)}
               />
-              <div className="w-full">
-                <label
-                  htmlFor="jobField"
-                  className="block font-semibold text-slate-600 mb-2 text-sm"
-                >
-                  Job Field
-                </label>
-                <div className="flex w-full">
-                  <div className="flex flex-col w-full">
-                    {!jobField ? (
-                      <input
-                        type="text"
-                        className="border rounded w-3/5 py-2 px-3 text-gray-700 leading-tight focus:outline-slate-400"
-                        id="jobField"
-                        placeholder="Search job fields"
-                        value={query}
-                        onChange={handleQueryChange}
-                      />
-                    ) : (
-                      <div className="flex">
-                        <input
-                          value={jobField}
-                          readOnly
-                          className="capitalize border rounded w-3/5 py-2 px-3 text-gray-700 leading-tight focus:outline-slate-400"
-                        />
-                        <button
-                          onClick={() => setJobField("")}
-                          className="m-auto w-1/6 ml-4 h-[80%] text-sm sm:text-sm border-2 bg-white border-slate-600 hover:bg-blue-200 text-slate-600 font-bold p-auto rounded focus:shadow-outline"
-                        >
-                          Change
-                        </button>
-                      </div>
-                    )}
-                    <div>
-                      {results && query && (
-                        <ul className="fixed z-10 bg-slate-200 w-1/4 h-40 overflow-auto capitalize">
-                          {results.map((item) => (
-                            <a
-                              key={item}
-                              href="/"
-                              onClick={(e) => handleSelect(e, item)}
-                            >
-                              <li className="hover:bg-slate-300">{item}</li>
-                            </a>
-                          ))}
-                        </ul>
-                      )}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="flex flex-col items-center justify-center w-1/3">
               <label
-                className="block font-semibold text-slate-600 mb-2 text-sm text-center"
-                htmlFor="isPublic"
+                htmlFor="jobField"
+                className="block font-semibold text-slate-400 mb-2 text-sm"
               >
-                Team Public?
+                JOB FIELD
               </label>
-              <input
-                id="isPublic"
-                type="checkbox"
-                defaultChecked={!isPrivate}
-                onChange={() => setIsPrivate(!isPrivate)}
-                className="w-5 h-5 mt-2"
-              />
+              {!jobField ? (
+                <input
+                  type="text"
+                  className="border border-black rounded w-3/5 py-2 px-3 text-gray-700 leading-tight focus:outline-slate-400"
+                  id="jobField"
+                  placeholder="Search job fields"
+                  value={query}
+                  onChange={handleQueryChange}
+                />
+              ) : (
+                <div className="flex">
+                  <input
+                    value={jobField}
+                    readOnly
+                    className="capitalize border border-black rounded w-3/5 py-2 px-3 text-gray-700 leading-tight focus:outline-slate-400"
+                  />
+                  <button
+                    onClick={() => setJobField("")}
+                    className="m-auto w-1/6 ml-4 h-[80%] text-sm sm:text-sm border-2 bg-white border-slate-600 hover:bg-blue-200 text-slate-600 font-bold p-auto rounded focus:shadow-outline"
+                  >
+                    Change
+                  </button>
+                </div>
+              )}
+              <div>
+                {results && query && (
+                  <ul className="fixed z-10 bg-slate-200 w-1/4 h-40 overflow-auto capitalize">
+                    {results.map((item) => (
+                      <a
+                        key={item}
+                        href="/"
+                        onClick={(e) => handleSelect(e, item)}
+                      >
+                        <li className="hover:bg-slate-300">{item}</li>
+                      </a>
+                    ))}
+                  </ul>
+                )}
+              </div>
+              <div className="w-fit">
+                <FormToggle
+                  id="isTeamPrivate"
+                  text="IS TEAM PRIVATE?"
+                  defaultChecked={isPrivate}
+                  handleChange={setIsPrivate}
+                />
+              </div>
             </div>
           </div>
           <div className="flex flex-col mt-3">
             <label
               htmlFor="credo"
-              className="block font-semibold text-slate-600 mb-2 text-sm"
+              className="block font-semibold text-slate-400 mb-2 text-sm"
             >
-              Team Credo
+              TEAM CREDO
             </label>
             <textarea
               id="description"
@@ -162,7 +150,7 @@ export const TeamSettingsPage = () => {
               placeholder={description || "Describe team and its focus..."}
               value={description}
               onChange={(e) => setDescription(e.target.value)}
-              className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-slate-400 resize-none"
+              className="border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-slate-400 resize-none"
               required={false}
             />
           </div>

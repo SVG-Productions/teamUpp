5723d903e2c1594ee4f0ab940905721da43bd279
Gino Ochoa <gochoa1088@gmail.com>
2023-05-16 13:41:38 -0700

    create team button adjustment

--- client/src/components/CreateTeamButton.js
+++ client/src/components/CreateTeamButton.js
@@ -5,7 +5,7 @@ const CreateTeamButton = ({ isCreateModalShowing, handleCreateModal }) => {
   return (
     <>
       <div
-        className="absolute flex justify-center right-0 top-1 w-8 h-8 text-xl 
+        className="flex justify-center right-0 top-1 w-8 h-8 text-xl 
      bg-white hover:bg-slate-400 rounded-full cursor-pointer sm:top-1.5"
         onClick={() => handleCreateModal(true)}
       >

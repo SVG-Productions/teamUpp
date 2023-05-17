3242f303402313efd4de694b4fe3e3ea080225cb
vincentochoa3 <vochoa893@gmail.com>
2023-05-17 12:56:46 -0700

    add ui updates for ListingTabs

--- client/src/components/ListingTabs.js
+++ client/src/components/ListingTabs.js
@@ -4,26 +4,30 @@ import { NavLink, useLoaderData } from "react-router-dom";
 const ListingTabs = () => {
   const { team, listing } = useLoaderData();
   return (
-    <div className="flex gap-3 w-1/4 px-2">
+    <div className="flex gap-5 w-1/4 mb-4">
       <NavLink
         className={({ isActive }) =>
-          `border-black pb-1 w-28 text-center ${
-            isActive ? "border-b-[3px] font-bold" : "border-b"
+          `pb-1 text-center font-bold text-lg ${
+            isActive
+              ? "border-b-[3px] text-blue-500 border-blue-500"
+              : "border-b text-slate-400 border-slate-400"
           }`
         }
         to={`/teams/${team.id}/listings/${listing.id}`}
       >
-        Details
+        DETAILS
       </NavLink>
       <NavLink
         className={({ isActive }) =>
-          `border-black pb-1 w-28 text-center ${
-            isActive ? "border-b-[3px] font-bold" : "border-b"
+          `pb-1 text-center font-bold text-lg ${
+            isActive
+              ? "border-b-[3px] text-blue-500 border-blue-500"
+              : "border-b text-slate-400 border-slate-400"
           }`
         }
         to={`/teams/${team.id}/listings/${listing.id}/experiences`}
       >
-        Experiences
+        EXPERIENCES
       </NavLink>
     </div>
   );

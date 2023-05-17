da7a2d272e78ebf45024b6469e9915d4b9340ce1
vincentochoa3 <vochoa893@gmail.com>
2023-05-17 13:53:45 -0700

    add details title for desktop view

--- client/src/components/ListingDetails.js
+++ client/src/components/ListingDetails.js
@@ -5,12 +5,15 @@ const ListingDetails = () => {
 
   return (
     <div className="flex flex-col gap-4 sm:w-7/12">
+      <p className="text-slate-400 text-lg font-bold hidden sm:block">
+        LISTING DETAILS
+      </p>
       <div>
-        <p className="font-bold text-slate-400">JOB TITLE</p>
+        <p className="font-bold text-slate-400">Job Title</p>
         <p>{listing.jobTitle}</p>
       </div>
       <div>
-        <p className="font-bold text-slate-400">LINK TO APPLY</p>
+        <p className="font-bold text-slate-400">Link to Apply</p>
         <a
           className="hover:underline"
           target="_blank"
@@ -21,15 +24,15 @@ const ListingDetails = () => {
         </a>
       </div>
       <div>
-        <p className="font-bold text-slate-400">COMPANY NAME</p>
+        <p className="font-bold text-slate-400">Company Name</p>
         <p>{listing.companyName}</p>
       </div>
       <div>
-        <p className="font-bold text-slate-400">COMPANY DETAILS</p>
+        <p className="font-bold text-slate-400">Company Details</p>
         <p>{listing.companyDetails}</p>
       </div>
       <div>
-        <p className="font-bold text-slate-400">JOB DESCRIPTION</p>
+        <p className="font-bold text-slate-400">Job Description</p>
         <p>{listing.jobDescription}</p>
       </div>
     </div>

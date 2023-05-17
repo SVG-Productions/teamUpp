9db5f76a7cf792942f5206fd6bf5deee442d10f2
vincentochoa3 <vochoa893@gmail.com>
2023-05-17 13:44:14 -0700

    change ListingTabs NavLinks to buttons and response based to state

--- client/src/components/ListingTabs.js
+++ client/src/components/ListingTabs.js
@@ -1,34 +1,32 @@
-import React from "react";
-import { NavLink, useLoaderData } from "react-router-dom";
+import { useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 const ListingTabs = () => {
   const { team, listing } = useLoaderData();
+  const [tabs, setTabs] = useState("details");
+
   return (
-    <div className="flex gap-5 w-1/4 mb-4">
-      <NavLink
-        className={({ isActive }) =>
-          `pb-1 text-center font-bold text-lg ${
-            isActive
-              ? "border-b-[3px] text-blue-500 border-blue-500"
-              : "border-b text-slate-400 border-slate-400"
-          }`
-        }
-        to={`/teams/${team.id}/listings/${listing.id}`}
+    <div className="flex gap-5 mb-4">
+      <button
+        className={`pb-1 w-28 text-center ${
+          tabs === "details"
+            ? "border-b-[3px] text-bluegray border-bluegray font-bold"
+            : "border-b text-slate-400 border-slate-400"
+        }`}
+        onClick={() => setTabs("details")}
       >
-        DETAILS
-      </NavLink>
-      <NavLink
-        className={({ isActive }) =>
-          `pb-1 text-center font-bold text-lg ${
-            isActive
-              ? "border-b-[3px] text-blue-500 border-blue-500"
-              : "border-b text-slate-400 border-slate-400"
-          }`
-        }
-        to={`/teams/${team.id}/listings/${listing.id}/experiences`}
+        Details
+      </button>
+      <button
+        className={`pb-1 w-28 text-center ${
+          tabs === "experiences"
+            ? "border-b-[3px] text-bluegray border-bluegray font-bold"
+            : "border-b text-slate-400 border-slate-400"
+        }`}
+        onClick={() => setTabs("experiences")}
       >
-        EXPERIENCES
-      </NavLink>
+        Experiences
+      </button>
     </div>
   );
 };

a44d9c402141c5b0308e8ca57315cfe9fb715152
Gino Ochoa <gochoa1088@gmail.com>
2023-05-20 00:31:13 -0700

    create form state for CreateExperienceModal

--- client/src/components/CreateExperienceModal.js
+++ client/src/components/CreateExperienceModal.js
@@ -6,8 +6,29 @@ import FormField from "./FormField";
 import ModalLayout from "./ModalLayout";
 
 const CreateExperienceModal = () => {
-  const [experienceTitle, setExperienceTitle] = useState("");
-  const [experienceDescription, setExperienceDescription] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [links, setLinks] = useState([]);
+  const [questions, setQuestions] = useState([]);
+
+  const { authedUser } = useAuth();
+  const userId = authedUser.id;
+
+  const { team, listing } = useLoaderData();
+  const { id: teamId, name } = team;
+  const { id: listingId, companyName, jobTitle } = listing;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const experienceData = {
+      title,
+      content,
+      userId,
+      listingId,
+    };
+    await axios.post("/api/experiences", experienceData);
+    navigate(`/teams/${teamId}/listings/${listingId}/experiences`);
+  };
   return <div></div>;
 };
 

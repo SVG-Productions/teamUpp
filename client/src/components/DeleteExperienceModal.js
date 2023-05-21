542e0c33bbec49072f355c7a268986b59183dcb6
Gino Ochoa <gochoa1088@gmail.com>
2023-05-20 22:37:25 -0700

    implement DeleteExperienceModal using new DeleteModalLayout

--- client/src/components/DeleteExperienceModal.js
+++ client/src/components/DeleteExperienceModal.js
@@ -1,60 +1,22 @@
 import axios from "axios";
-import { useState } from "react";
 import { useLoaderData, useNavigate } from "react-router-dom";
+import DeleteModalLayout from "./DeleteModalLayout";
 
-const DeleteExperienceModal = ({ isOpen, onClose }) => {
+const DeleteExperienceModal = ({ handleModal }) => {
   const { team, listing, experience } = useLoaderData();
-  const [isDeleting, setIsDeleting] = useState(false);
   const navigate = useNavigate();
 
   const handleDeleteExperience = async () => {
-    setIsDeleting(true);
     await axios.delete(`/api/experiences/${experience.id}`);
-    setIsDeleting(false);
-    onClose();
-    navigate(`/teams/${team.id}/listings/${listing.id}/experiences`);
+    navigate(`/teams/${team.id}/listings/${listing.id}`);
   };
 
   return (
-    <div
-      className={`fixed inset-0 z-50 ${isOpen ? "" : "hidden"} overflow-y-auto`}
-    >
-      <div className="flex items-center justify-center min-h-screen">
-        <div
-          className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
-          onClick={onClose}
-        ></div>
-
-        <div className="relative bg-white w-full max-w-sm mx-auto rounded-sm shadow-lg z-10">
-          <div className="p-4">
-            <h2 className="text-lg font-medium mb-4 text-center">
-              Delete this experience?
-            </h2>
-            <p className="text-sm">
-              You're about to permanently delete this experience and all of its
-              data.
-            </p>
-
-            <div className="flex justify-center mt-6 gap-3">
-              <button
-                className="bg-red-400 hover:bg-red-500 text-white font-medium px-4 py-2 rounded-sm"
-                onClick={handleDeleteExperience}
-                disabled={isDeleting}
-              >
-                {isDeleting ? "Deleting..." : "Delete"}
-              </button>
-              <button
-                className="bg-gray-200 hover:bg-gray-300 text-gray-700 font-medium px-4 py-2 mr-2 rounded-sm"
-                onClick={onClose}
-                disabled={isDeleting}
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <DeleteModalLayout
+      content="experience"
+      handleDelete={handleDeleteExperience}
+      handleModal={handleModal}
+    />
   );
 };
 

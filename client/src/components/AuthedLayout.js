7e82cc0cffc152204eaacd24465e67b060266e2a
Gino Ochoa <gochoa1088@gmail.com>
2023-05-22 19:15:14 -0700

    implement reac top loading bar

--- client/src/components/AuthedLayout.js
+++ client/src/components/AuthedLayout.js
@@ -3,16 +3,25 @@ import {
   Outlet,
   useNavigation,
   ScrollRestoration,
+  useLocation,
 } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import AuthedPageContainer from "./AuthedPageContainer";
 import { useAuth } from "../context/AuthContext";
-import LoadingSpinner from "./LoadingSpinner";
+import LoadingBar from "react-top-loading-bar";
+import { useEffect, useState } from "react";
 
 const AuthedLayout = ({ children }) => {
   const { authedUser } = useAuth();
   const navigation = useNavigation();
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    if (navigation.state === "idle") setProgress(100);
+    if (navigation.state === "submitting") setProgress(30);
+    if (navigation.state === "loading") setProgress(progress + 30);
+  }, [navigation.state]);
 
   if (!authedUser) {
     return <Navigate to="/login" />;
@@ -21,7 +30,12 @@ const AuthedLayout = ({ children }) => {
   return (
     <>
       <ScrollRestoration />
-      {navigation.state === "loading" && <LoadingSpinner />}
+      <LoadingBar
+        color="#0F172A"
+        height={4}
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
       <div className="flex flex-col min-h-screen items-center bg-white">
         <Navbar />
         <AuthedPageContainer>{children || <Outlet />}</AuthedPageContainer>

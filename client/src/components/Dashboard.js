0f4eba939be52bc5a216c3d38120794b3d7bd23d
vincentochoa3 <vochoa893@gmail.com>
2023-05-24 15:28:14 -0700

    add teams and teammates list components to Dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,15 +1,16 @@
-import { NavLink, useLoaderData, useNavigate } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useAuth } from "../context/AuthContext";
-import ScrollableList from "./ScrollableList";
 import AuthedPageTitle from "./AuthedPageTitle";
 import AcceptButton from "./AcceptButton";
 import DenyButton from "./DenyButton";
 import RecentActivity from "./RecentActivity";
+import NullInfo from "../components/NullInfo";
+import UserTeamsList from "./UserTeamsList";
+import UserTeammatesList from "../components/UserTeammatesList";
 
 const Dashboard = () => {
-  const { userTeams, invites, teammates, recommendedTeams, recentActivity } =
-    useLoaderData();
+  const { invites, recentActivity } = useLoaderData();
   const { authedUser } = useAuth();
   const navigate = useNavigate();
 
@@ -37,13 +38,13 @@ const Dashboard = () => {
         ]}
       />
       <div className="flex flex-col sm:flex-row gap-10 my-8 h-full min-h-[410px] px-10">
-        <div className="flex flex-col sm:w-[75%]">
-          <div className="sm:w-full md:w-[50%]">
-            <p className="font-bold text-slate-400 text-2xl">NOTIFICATIONS</p>
+        <div className="flex flex-col sm:w-3/4">
+          <div className="">
+            <p className="font-bold text-slate-400">NOTIFICATIONS</p>
             {invites.length ? (
               invites.map((team, index) => (
                 <li
-                  className="flex bg-white p-2.5 rounded-md justify-between hover:bg-highlightblue"
+                  className="flex bg-white p-2.5 rounded-sm justify-between hover:bg-highlightblue"
                   key={`${team.name}-${index}`}
                 >
                   <span>Invite to join {team.name}!</span>
@@ -60,44 +61,23 @@ const Dashboard = () => {
                 </li>
               ))
             ) : (
-              <p className="p-8">No Notifications</p>
+              <NullInfo />
             )}
           </div>
           <div className="sm:h-[60%] pt-10">
-            <p className="font-bold text-slate-400 text-2xl">RECENT ACTIVITY</p>
+            <p className="font-bold text-slate-400">RECENT ACTIVITY</p>
             {recentActivity.map((activity, index) => (
               <RecentActivity activity={activity} index={index} />
             ))}
           </div>
-          <div className="flex flex-col sm:flex-row sm:h-[35%] justify-between"></div>
         </div>
-        <div className="hidden sm:flex flex-col sm:w-[25%] justify-between min-w-[180px]">
-          <div className="flex flex-col sm:w-full">
-            <p className="font-bold text-slate-400 text-2xl">YOUR TEAMS</p>
-            {userTeams.map((team, index) => (
-              <NavLink
-                to={`/teams/${team.id}`}
-                className="bg-white p-2.5 rounded-md hover:bg-highlightblue"
-                key={`${team.name}-${index}`}
-              >
-                {team.name}
-              </NavLink>
-            ))}
+        <div className="flex flex-col sm:w-1/4 gap-8">
+          <div>
+            <UserTeamsList heading="YOUR TEAMS" />
           </div>
-          <div className="sm:w-full">
-            <p className="font-bold text-slate-400 text-2xl pt-10">
-              YOUR TEAMMATES
-            </p>
-            {teammates.map((teammate, index) => (
-              <NavLink
-                to={`/${teammate.username}`}
-                className="flex p-2.5 rounded-sm hover:bg-highlightblue"
-                key={`${teammate.id}-${index}`}
-              >
-                <div className="bg-white rounded-full w-6 h-6 mr-4" />
-                <p> {teammate.username}</p>
-              </NavLink>
-            ))}
+          <div>
+            {" "}
+            <UserTeammatesList />
           </div>
         </div>
       </div>

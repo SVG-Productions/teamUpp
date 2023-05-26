8944b8b05f69a43edf39ce076992a8a19a663982
Gino Ochoa <gochoa1088@gmail.com>
2023-05-25 17:26:48 -0700

    desktop UserSettingsLayout done

--- client/src/components/UserSettingsLayout.js
+++ client/src/components/UserSettingsLayout.js
@@ -1,27 +1,70 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLoaderData } from "react-router-dom";
+import AuthedPageTitle from "./AuthedPageTitle";
 
 const UserSettingsLayout = ({ children }) => {
+  const { user } = useLoaderData();
   return (
-    <div className="flex sm:flex-col">
-      <div id="topBar" className="flex justify-between">
-        <div className="flex items-center">
-          <div></div>
-          <h1>
-            <NavLink></NavLink>
-          </h1>
+    <>
+      <AuthedPageTitle
+        links={[
+          { to: `/${user.username}`, label: user.username },
+          { label: "Settings" },
+        ]}
+      />
+      <div className="flex self-center w-full sm:flex-col sm:max-w-7xl sm:p-2 sm:pb-8">
+        <div id="topBar" className="flex justify-between items-center mt-4">
+          <div className="flex items-center">
+            <div
+              className=" flex items-center justify-center bg-slate-900 rounded-full w-10 h-10 
+            text-xs font-semibold text-white mr-3"
+            >
+              UI
+            </div>
+            <h1>
+              <NavLink
+                to={`/${user.username}`}
+                className="no-underline font-semibold text-slate-900 hover:underline"
+              >
+                {user.firstName}
+                <span className="text-slate-600"> ({user.username})</span>
+              </NavLink>
+            </h1>
+          </div>
+          <NavLink
+            to={`/${user.username}`}
+            className="no-underline font-semibold px-2 rounded-md text-slate-600
+          border-2 border-slate-400 hover:border-slate-600 hover:bg-slate-200"
+          >
+            Go to profile
+          </NavLink>
         </div>
-        <NavLink>Go to profile</NavLink>
-      </div>
-      <div id="mainGroup" className="flex">
-        <div id="sidebar" className="flex flex-col pr-4">
-          <NavLink>Public profile</NavLink>
-          <NavLink>Account</NavLink>
-          <NavLink>Appearance</NavLink>
+        <div id="mainGroup" className="flex w-full">
+          <div
+            id="sidebar"
+            className="flex flex-col py-6 sm:w-[180px] md:w-[240px]"
+          >
+            <div
+              id="firstGroup"
+              className="flex flex-col border-b border-slate-300 pb-2"
+            >
+              <NavLink className="no-underline text-slate-900 font-semibold rounded-md w-full px-1 py-0.5 hover:bg-slate-200">
+                Public profile
+              </NavLink>
+              <NavLink className="no-underline text-slate-900 font-semibold rounded-md w-full px-1 py-0.5 hover:bg-slate-200">
+                Account
+              </NavLink>
+              <NavLink className="no-underline text-slate-900 font-semibold rounded-md w-full px-1 py-0.5 hover:bg-slate-200">
+                Appearance
+              </NavLink>
+            </div>
+          </div>
+          <div id="main" className="w-full sm:pl-4 sm:pt-6 md:pl-8 lg:pl-16">
+            {children}
+          </div>
         </div>
-        <div id="main">{children}</div>
       </div>
-    </div>
+    </>
   );
 };
 
--- client/src/pages/UserSettingsPage.js
+++ client/src/pages/UserSettingsPage.js
@@ -10,6 +10,7 @@ import DeleteAccountModal from "../components/DeleteAccountModal";
 import ReactQuill from "react-quill";
 import { basicModules } from "../utils/quillModules";
 import "react-quill/dist/quill.snow.css";
+import UserSettingsLayout from "../components/UserSettingsLayout";
 
 export const UserSettingsPage = () => {
   const { user, jobFields: fields } = useLoaderData();
@@ -43,12 +44,6 @@ export const UserSettingsPage = () => {
 
   return (
     <>
-      <AuthedPageTitle
-        links={[
-          { to: `/${user.username}`, label: user.username },
-          { label: "Settings" },
-        ]}
-      />
       {/* <button
           className="border-2 rounded justify-center self-center text-xs 
       font-bold text-red-500 bg-white border-red-500 hover:bg-red-200 p-2 mt-1 whitespace-nowrap"
@@ -60,114 +55,119 @@ export const UserSettingsPage = () => {
       {/* {isDeleteModalOpen && (
         <DeleteAccountModal handleModal={setIsDeleteModalOpen} />
       )} */}
-      <form
-        className={`flex flex-col flex-grow self-center w-full 
+      <UserSettingsLayout>
+        <form
+          className={`flex flex-col flex-grow self-center w-full 
           ${isDeleteModalOpen && "max-h-[calc(100vh-12rem)] overflow-hidden"}
-          rounded-sm p-6 max-w-4xl sm:py-4 sm:px-12 sm:pt-8 sm:max-h-full`}
-        onSubmit={handleSubmit}
-      >
-        <div className="flex flex-col-reverse justify-between sm:flex-row">
-          <div className="w-full sm:w-1/2">
-            <div className="flex flex-col justify-between sm:flex-row sm:gap-4">
-              <FormField
-                label="FIRST NAME"
-                id="firstName"
-                type="text"
-                placeholder={firstName}
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-                required={false}
-              />
-              <FormField
-                label="LAST NAME"
-                id="lastName"
-                type="text"
-                placeholder={lastName}
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-                required={false}
-              />
-            </div>
-            <div className="flex justify-between">
-              <div className="w-2/3">
+          rounded-sm max-w-6xl sm:max-h-full`}
+          onSubmit={handleSubmit}
+        >
+          <h1 className="text-slate-400 font-semibold pb-2 mb-4 border-b border-slate-300">
+            Public profile
+          </h1>
+          <div className="flex flex-col-reverse justify-between sm:flex-row">
+            <div className="w-full sm:w-1/2">
+              <div className="flex flex-col justify-between sm:flex-row sm:gap-4">
                 <FormField
-                  label="EMAIL"
-                  id="email"
+                  label="FIRST NAME"
+                  id="firstName"
                   type="text"
-                  placeholder={email}
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder={firstName}
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  required={false}
+                />
+                <FormField
+                  label="LAST NAME"
+                  id="lastName"
+                  type="text"
+                  placeholder={lastName}
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  required={false}
                 />
               </div>
-              <div className="flex flex-col items-center w-1/3">
-                <FormToggle
-                  id="isPublic"
-                  text="EMAIL PUBLIC?"
-                  defaultChecked={isEmailPublic}
-                  handleChange={setIsEmailPublic}
-                />
+              <div className="flex justify-between">
+                <div className="w-2/3">
+                  <FormField
+                    label="EMAIL"
+                    id="email"
+                    type="text"
+                    placeholder={email}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="flex flex-col items-center w-1/3">
+                  <FormToggle
+                    id="isPublic"
+                    text="EMAIL PUBLIC?"
+                    defaultChecked={isEmailPublic}
+                    handleChange={setIsEmailPublic}
+                  />
+                </div>
               </div>
+              <FormField
+                label="LINKEDIN"
+                id="linkedIn"
+                type="text"
+                placeholder={linkedin}
+                value={linkedin}
+                onChange={(e) => setLinkedin(e.target.value)}
+                required={false}
+              />
+              <FormField
+                label="GITHUB"
+                id="github"
+                type="text"
+                placeholder={github}
+                value={github}
+                onChange={(e) => setGithub(e.target.value)}
+                required={false}
+              />
             </div>
-            <FormField
-              label="LINKEDIN"
-              id="linkedIn"
-              type="text"
-              placeholder={linkedin}
-              value={linkedin}
-              onChange={(e) => setLinkedin(e.target.value)}
-              required={false}
-            />
-            <FormField
-              label="GITHUB"
-              id="github"
-              type="text"
-              placeholder={github}
-              value={github}
-              onChange={(e) => setGithub(e.target.value)}
-              required={false}
+            <div className="flex flex-col items-center w-full mb-8 sm:w-1/2 sm:ml-12 sm:mb-0">
+              <UserSettingsProfilePicture />
+            </div>
+          </div>
+          <div className="w-full mb-2 flex">
+            <UserSettingsInterests
+              selectedItems={selectedItems}
+              setSelectedItems={setSelectedItems}
             />
           </div>
-          <div className="flex flex-col items-center w-full mb-8 sm:w-1/2 sm:ml-12 sm:mb-0">
-            <UserSettingsProfilePicture />
+          <div className="flex flex-col">
+            <label
+              htmlFor="readMe"
+              className="block font-bold text-slate-400 mb-2 text-sm"
+            >
+              README
+            </label>
+            <ReactQuill
+              id="readMe"
+              value={readme}
+              onChange={setReadme}
+              modules={basicModules}
+              theme="snow"
+            />
           </div>
-        </div>
-        <div className="w-full mb-2 flex">
-          <UserSettingsInterests
-            selectedItems={selectedItems}
-            setSelectedItems={setSelectedItems}
-          />
-        </div>
-        <div className="flex flex-col">
-          <label
-            htmlFor="readMe"
-            className="block font-bold text-slate-400 mb-2 text-sm"
-          >
-            README
-          </label>
-          <ReactQuill
-            id="readMe"
-            value={readme}
-            onChange={setReadme}
-            modules={basicModules}
-            theme="snow"
-          />
-        </div>
-        <div className="flex justify-center align-center gap-5 mt-5 sm:justify-end">
-          <button
-            className="w-1/4 min-w-[84px] text-sm bg-bluegray hover:bg-blue-900 text-white 
-              font-bold py-2 px-4 rounded-md focus:shadow-outline sm:w-1/6 sm:text-base"
-          >
-            Save
-          </button>
-          <NavLink
-            to={`/${user.username}`}
-            className="w-1/4 min-w-[84px] no-underline text-sm text-center bg-white hover:bg-gray-300 border-2 
-              text-black font-bold py-2 px-4 rounded-md focus:shadow-outline sm:w-1/6 sm:text-base"
-          >
-            Cancel
-          </NavLink>
-        </div>
-      </form>
+          <div className="flex justify-center align-center gap-5 mt-5 sm:justify-end">
+            <button
+              className="w-1/4 min-w-[84px] text-sm bg-bluegray hover:bg-blue-900 text-white 
+            font-bold py-2 px-4 rounded-md focus:shadow-outline sm:w-1/6 sm:text-base"
+            >
+              Save
+            </button>
+            <NavLink
+              to={`/${user.username}`}
+              className="w-1/4 min-w-[84px] no-underline text-sm text-center bg-white hover:bg-gray-300 border-2 
+            text-black font-bold py-2 px-4 rounded-md focus:shadow-outline sm:w-1/6 sm:text-base"
+            >
+              Cancel
+            </NavLink>
+          </div>
+        </form>
+      </UserSettingsLayout>
     </>
   );
 };

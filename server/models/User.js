60fda37e5fafe4b67ad7e9905f821c63df29b7ab
Gino Ochoa <gochoa1088@gmail.com>
2023-05-30 11:43:34 -0700

    add photo and avatar to getRecommendedTeams model

--- server/models/User.js
+++ server/models/User.js
@@ -146,7 +146,14 @@ const getRecommendedTeams = async (userId) => {
 
     const recommendedTeams = await knex("users_teams")
       .join("teams", "users_teams.team_id", "=", "teams.id")
-      .select("teams.id", "teams.name", "teams.job_field", "teams.description")
+      .select(
+        "teams.id",
+        "teams.name",
+        "teams.job_field",
+        "teams.description",
+        "teams.avatar",
+        "teams.photo"
+      )
       .whereIn(
         "user_id",
         knex("users_teams")

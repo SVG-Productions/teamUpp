1375e2c6ea101668ffd5281786a54f8858dcac6d
vincentochoa3 <vochoa893@gmail.com>
2023-06-01 14:14:30 -0700

    adjust position of nav dropdown

--- client/src/components/NavDropdownList.js
+++ client/src/components/NavDropdownList.js
@@ -15,8 +15,8 @@ const NavDropdownList = ({ setIsListShowing }) => {
   };
 
   return (
-    <div className="absolute flex flex-col top-1 -right-3 z-30">
-      <div className="w-0 h-0 self-end mr-9 border-8 border-borderprimary border-t-0 border-l-transparent border-r-transparent" />
+    <div className="absolute flex flex-col top-0.5 right-0.5 z-30">
+      <div className="w-0 h-0 self-end mr-6 border-8 border-borderprimary border-t-0 border-l-transparent border-r-transparent" />
       <div className="flex flex-col w-40 bg-secondary border border-borderprimary rounded-[2%] text-sm shadow-md">
         <span className="border-b border-borderprimary p-2">
           Signed in as <span className="font-bold">{authedUser.username}</span>

23d3c5ca9a2106843375c33c8b7d27c319d5bc6f
vincentochoa3 <vochoa893@gmail.com>
2023-06-01 12:46:00 -0700

    add color variables and adjust coloring on Navbar buttons/links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,6 +4,8 @@ import { useAuth } from "../context/AuthContext";
 import NavDropdownList from "./NavDropdownList";
 import useOnClickOutside from "../hooks/useOnClickOutside";
 import LogoSmall from "./LogoSmall";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   const { authedUser } = useAuth();
@@ -14,32 +16,27 @@ const Navbar = () => {
   return (
     <div className="sticky top-0 z-30 p-4 w-full flex items-center h-16 justify-between bg-secondary shadow-[0_1px_3px_rgb(0,0,0,0.2)]">
       <div className="flex gap-4 sm:gap-8 items-center">
-        <NavLink to="/" className="no-underline">
+        <NavLink to="/" className="no-underline hover:bg">
           <LogoSmall />
         </NavLink>
         <NavLink
           to="/teams"
-          className="text-primary no-underline text-sm font-semibold sm:text-base"
+          className="text-primary no-underline text-sm font-semibold hover:text-secondary sm:text-base"
         >
           Teams
         </NavLink>
         <NavLink
           to={`/${authedUser?.username}/favorites`}
-          className="text-primary no-underline text-sm font-semibold sm:text-base"
+          className="text-primary no-underline text-sm font-semibold hover:text-secondary sm:text-base"
         >
           Favorites
         </NavLink>
       </div>
       <div ref={navButtonRef}>
         <button
-          className="flex items-center gap-1"
+          className="flex items-center gap-2"
           onClick={() => setIsListShowing(isListShowing ? false : true)}
         >
-          {isListShowing ? (
-            <div className="text-[#cbd5e1]">&#9650;</div>
-          ) : (
-            <div className="text-[#cbd5e1]">&#9660;</div>
-          )}
           <img
             className="rounded-full"
             src={authedUser.photo || authedUser.avatar}
@@ -47,6 +44,7 @@ const Navbar = () => {
             height={40}
             alt={authedUser.username}
           />
+          <FontAwesomeIcon icon={faCaretDown} size="xs" />
         </button>
         <div className="relative w-full">
           {isListShowing && (

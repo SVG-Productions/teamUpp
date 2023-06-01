eb37974859d18acb8fe30e5a7e767d694cb3f6a6
Gino Ochoa <gochoa1088@gmail.com>
2023-06-01 15:14:53 -0700

    import user avatars to profile photo settings page

--- client/src/pages/UserPhotoSettingsPage.js
+++ client/src/pages/UserPhotoSettingsPage.js
@@ -1,6 +1,10 @@
-import React from "react";
+import { useRouteLoaderData } from "react-router-dom";
+import userAvatars from "../utils/userAvatars";
 
 export const UserPhotoSettingsPage = () => {
+  const { userData } = useRouteLoaderData("userSettings");
+  const { avatar, photo } = userData;
+
   return (
     <div
       className="flex flex-col flex-grow self-center w-full
@@ -8,8 +12,11 @@ export const UserPhotoSettingsPage = () => {
     >
       <div className="w-full">
         <h1 className="text-headingColor font-semibold pb-2 mb-4 border-b border-borderprimary">
-          Profile photo -- Coming soon!
+          Profile photo
         </h1>
+        {userAvatars.map((ua) => (
+          <img key={ua} src={ua} />
+        ))}
       </div>
     </div>
   );

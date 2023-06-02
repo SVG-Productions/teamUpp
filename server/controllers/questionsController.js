aa58fe5d3e238eda916ef3a4f02a29ab2f5f512a
Gino Ochoa <gochoa1088@gmail.com>
2023-06-02 14:41:16 -0700

    update some status codes

--- server/controllers/questionsController.js
+++ server/controllers/questionsController.js
@@ -3,7 +3,7 @@ const Question = require("../models/Question");
 const addQuestion = async (req, res, next) => {
   try {
     const question = await Question.addQuestions(req.body);
-    res.status(200).json(question);
+    res.status(201).json(question);
   } catch (error) {
     next(error);
   }

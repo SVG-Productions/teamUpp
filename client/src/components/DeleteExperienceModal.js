f0fb4b6078c02484b9252ec867e03cb44ed2f09d
Gino Ochoa <gochoa1088@gmail.com>
2023-06-02 18:30:46 -0700

    add toast to DeleteExperienceModal

--- client/src/components/DeleteExperienceModal.js
+++ client/src/components/DeleteExperienceModal.js
@@ -1,15 +1,21 @@
 import axios from "axios";
 import { useLoaderData, useNavigate } from "react-router-dom";
 import DeleteModalLayout from "../layouts/DeleteModalLayout";
+import { toast } from "react-hot-toast";
+import { basicToast } from "../utils/toastOptions";
 
 const DeleteExperienceModal = ({ handleModal }) => {
   const { teamData, listingData, experienceData } = useLoaderData();
   const navigate = useNavigate();
 
   const handleDeleteExperience = async () => {
-    await axios.delete(`/api/experiences/${experienceData.id}`);
-    handleModal(false);
-    navigate(`/teams/${teamData.id}/listings/${listingData.id}`);
+    try {
+      await axios.delete(`/api/experiences/${experienceData.id}`);
+      handleModal(false);
+      navigate(`/teams/${teamData.id}/listings/${listingData.id}`);
+    } catch (error) {
+      toast.error("Oops. Problem deleting experience.", basicToast);
+    }
   };
 
   return (

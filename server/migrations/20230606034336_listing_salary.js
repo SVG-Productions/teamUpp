ffa08abaa146d9611978071d33f162f8a452efe4
Gino Ochoa <gochoa1088@gmail.com>
2023-06-05 21:22:32 -0700

    salary migration

---
+++ server/migrations/20230606034336_listing_salary.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+  await knex.schema.alterTable("listings", function (table) {
+    table.decimal("salary_amount", null);
+    table.string("salary_frequency");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+  await knex.schema.alterTable("listings", function (table) {
+    table.dropColumn("salary_amount");
+    table.dropColumn("salary_frequency");
+  });
+};

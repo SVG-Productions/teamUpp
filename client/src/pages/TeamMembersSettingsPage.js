bba0be2d47be21b1a66531da962941bf57fb52b6
Gino Ochoa <gochoa1088@gmail.com>
2023-06-07 16:10:41 -0700

    render teammates and requested members as list

--- client/src/pages/TeamMembersSettingsPage.js
+++ client/src/pages/TeamMembersSettingsPage.js
@@ -1,5 +1,6 @@
 import { NavLink, useRouteLoaderData } from "react-router-dom";
 import InviteTeammateForm from "../components/InviteTeammateForm";
+import NullInfo from "../components/NullInfo";
 
 export const TeamMembersSettingsPage = () => {
   const { teamData } = useRouteLoaderData("teamSettings");
@@ -9,6 +10,58 @@ export const TeamMembersSettingsPage = () => {
       className="flex flex-col flex-grow self-center w-full
   rounded-sm max-w-6xl sm:max-h-full"
     >
+      <div className="flex flex-col w-full gap-4">
+        <h1 className="text-headingColor font-semibold pb-2 mb-4 border-b border-borderprimary">
+          Manage members
+        </h1>
+        <ul className="grid w-full grid-cols-1 gap-y-1 mb-6 sm:grid-cols-2 lg:grid-cols-3">
+          {teamData.teammates.map((i) => (
+            <li key={i.id} className="w-[80%] px-1 hover:bg-highlight">
+              <NavLink
+                to={`/${i.username}`}
+                className="w-full flex self-center gap-1 no-underline text-primary p-2.5 rounded-sm sm:px-1"
+              >
+                <img
+                  className="rounded-full mr-2"
+                  src={i.photo || i.avatar}
+                  width={28}
+                  height={28}
+                  alt={i.username}
+                />
+                <span className="truncate">{i.username}</span>
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="flex flex-col w-full gap-4">
+        <h1 className="text-headingColor font-semibold pb-2 mb-4 border-b border-borderprimary">
+          Manage requests
+        </h1>
+        <ul className="grid w-full grid-cols-1 gap-y-1 mb-6 sm:grid-cols-2 lg:grid-cols-3">
+          {teamData.requested.length ? (
+            teamData.requested.map((i) => (
+              <li key={i.id} className="w-[80%] px-1 hover:bg-highlight">
+                <NavLink
+                  to={`/${i.username}`}
+                  className="w-full flex self-center gap-1 no-underline text-primary p-2.5 rounded-sm sm:px-1"
+                >
+                  <img
+                    className="rounded-full mr-2"
+                    src={i.photo || i.avatar}
+                    width={28}
+                    height={28}
+                    alt={i.username}
+                  />
+                  <span className="truncate">{i.username}</span>
+                </NavLink>
+              </li>
+            ))
+          ) : (
+            <NullInfo />
+          )}
+        </ul>
+      </div>
       <div className="flex flex-col w-full gap-4">
         <h1 className="text-headingColor font-semibold pb-2 mb-4 border-b border-borderprimary">
           Manage invites
@@ -22,10 +75,10 @@ export const TeamMembersSettingsPage = () => {
             <h3 className="text-headingColor">Invited members:</h3>
             <ul className="grid w-full grid-cols-1 gap-y-1 my-6 sm:grid-cols-2 lg:grid-cols-3">
               {teamData.invited.map((i) => (
-                <li key={i.id} className="w-[80%] hover:bg-highlight">
+                <li key={i.id} className="w-[80%] px-1 hover:bg-highlight">
                   <NavLink
                     to={`/${i.username}`}
-                    className="w-full flex items-center gap-1 no-underline text-primary p-2.5 rounded-sm sm:px-1"
+                    className="w-full flex self-center gap-1 no-underline text-primary p-2.5 rounded-sm sm:px-1"
                   >
                     <img
                       className="rounded-full mr-2"
@@ -41,16 +94,6 @@ export const TeamMembersSettingsPage = () => {
             </ul>
           </div>
         )}
-      </div>
-      <div className="flex flex-col w-full gap-4">
-        <h1 className="text-headingColor font-semibold pb-2 mb-4 border-b border-borderprimary">
-          Manage members
-        </h1>
-      </div>
-      <div className="flex flex-col w-full gap-4">
-        <h1 className="text-headingColor font-semibold pb-2 mb-4 border-b border-borderprimary">
-          Manage requests
-        </h1>
       </div>
     </div>
   );

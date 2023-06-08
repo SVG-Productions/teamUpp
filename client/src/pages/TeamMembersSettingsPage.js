4bc9141914fab1b24a1bc2367cb43601808acc2c
Gino Ochoa <gochoa1088@gmail.com>
2023-06-07 21:47:53 -0700

    implement accept/deny requests

--- client/src/pages/TeamMembersSettingsPage.js
+++ client/src/pages/TeamMembersSettingsPage.js
@@ -1,9 +1,49 @@
-import { NavLink, useRouteLoaderData } from "react-router-dom";
+import { NavLink, useRevalidator, useRouteLoaderData } from "react-router-dom";
 import InviteTeammateForm from "../components/InviteTeammateForm";
 import NullInfo from "../components/NullInfo";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSquareCheck,
+  faSquareXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+import { basicToast } from "../utils/toastOptions";
+import axios from "axios";
 
 export const TeamMembersSettingsPage = () => {
   const { teamData } = useRouteLoaderData("teamSettings");
+  const revalidator = useRevalidator();
+
+  const [openRequestMenu, setOpenRequestMenu] = useState(null);
+  const handleRequestMenuClick = (index) => {
+    setOpenRequestMenu(index === openRequestMenu ? null : index);
+  };
+
+  const handleAcceptRequest = async (userId) => {
+    try {
+      await axios.patch(`/api/teams/${teamData.id}/teammates`, {
+        userId,
+        status: "member",
+      });
+      revalidator.revalidate();
+      toast.success("Request successfully accepted!", basicToast);
+    } catch (error) {
+      toast.error("Error accepting request.", basicToast);
+    }
+  };
+
+  const handleDenyRequest = async (userId) => {
+    try {
+      await axios.delete(`/api/teams/${teamData.id}/teammates`, {
+        data: { userId },
+      });
+      revalidator.revalidate();
+      toast.success("Request successfully denied.", basicToast);
+    } catch (error) {
+      toast.error("Error denying request.", basicToast);
+    }
+  };
 
   return (
     <div
@@ -36,25 +76,42 @@ export const TeamMembersSettingsPage = () => {
       </div>
       <div className="flex flex-col w-full gap-4">
         <h1 className="text-headingColor font-semibold pb-2 mb-4 border-b border-borderprimary">
-          Incoming requests
+          Requests to join
         </h1>
         <ul className="grid w-full grid-cols-1 gap-y-1 mb-6 sm:grid-cols-2 lg:grid-cols-3">
           {teamData.requested.length ? (
-            teamData.requested.map((i) => (
-              <li key={i.id} className="w-[80%] px-1 hover:bg-highlight">
+            teamData.requested.map((r, index) => (
+              <li
+                key={r.id}
+                className="flex w-[80%] items-center px-1 hover:bg-highlight"
+              >
                 <NavLink
-                  to={`/${i.username}`}
-                  className="w-full flex self-center gap-1 no-underline text-primary p-2.5 rounded-sm sm:px-1"
+                  to={`/${r.username}`}
+                  className="w-full flex items-center gap-1 no-underline text-primary p-2.5 rounded-sm sm:px-1"
                 >
                   <img
                     className="rounded-full mr-2"
-                    src={i.photo || i.avatar}
+                    src={r.photo || r.avatar}
                     width={28}
                     height={28}
-                    alt={i.username}
+                    alt={r.username}
                   />
-                  <span className="truncate">{i.username}</span>
+                  <span className="truncate">{r.username}</span>
                 </NavLink>
+                <div className="flex gap-2">
+                  <FontAwesomeIcon
+                    size="lg"
+                    className="text-iconPrimary cursor-pointer hover:text-green-500"
+                    icon={faSquareCheck}
+                    onClick={() => handleAcceptRequest(r.id)}
+                  />
+                  <FontAwesomeIcon
+                    size="lg"
+                    className="text-iconPrimary cursor-pointer hover:text-red-500"
+                    icon={faSquareXmark}
+                    onClick={() => handleDenyRequest(r.id)}
+                  />
+                </div>
               </li>
             ))
           ) : (
@@ -64,7 +121,7 @@ export const TeamMembersSettingsPage = () => {
       </div>
       <div className="flex flex-col w-full gap-4">
         <h1 className="text-headingColor font-semibold pb-2 mb-4 border-b border-borderprimary">
-          Outgoing invites
+          Invites outgoing
         </h1>
         <h3 className="text-headingColor">
           Invite a friend to join {teamData.name}!

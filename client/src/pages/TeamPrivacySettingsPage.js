ef4ef4ddd6126618be5ae92226d79921adf823b5
Gino Ochoa <gochoa1088@gmail.com>
2023-06-08 14:12:54 -0700

    basic layout

--- client/src/pages/TeamPrivacySettingsPage.js
+++ client/src/pages/TeamPrivacySettingsPage.js
@@ -1,6 +1,28 @@
-import React from "react";
+import { useState } from "react";
+import { useRevalidator, useRouteLoaderData } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { basicToast } from "../utils/toastOptions";
 
 export const TeamPrivacySettingsPage = () => {
+  const { teamData } = useRouteLoaderData("teamSettings");
+
+  const [isPrivate, setIsPrivate] = useState(teamData.isPrivate);
+
+  const revalidator = useRevalidator();
+
+  const handleSubmit = async (e) => {
+    try {
+      const updates = { isPrivate };
+      await axios.patch(`/api/teams/${teamData.id}`, updates);
+
+      revalidator.revalidate();
+      toast.success("Team successfully updated!", basicToast);
+    } catch (error) {
+      toast.error("Oops! Something went wrong.", basicToast);
+    }
+  };
+
   return (
     <div
       className="flex flex-col flex-grow self-center w-full
@@ -8,8 +30,36 @@ export const TeamPrivacySettingsPage = () => {
     >
       <div className="w-full">
         <h1 className="text-headingColor font-semibold pb-2 mb-4 border-b border-borderprimary">
-          Privacy -- Coming soon!
+          Privacy
         </h1>
+        <div className="flex justify-between mb-4">
+          <div className="flex flex-col">
+            <p className="font-bold">Change team visibility</p>
+            <p>
+              This team is currently{" "}
+              {isPrivate
+                ? "private and cannot be searched"
+                : "public and can be searched"}
+              .
+            </p>
+          </div>
+          {isPrivate ? (
+            <button>Make public</button>
+          ) : (
+            <button>Make private</button>
+          )}
+        </div>
+        <div className="flex justify-between">
+          <div className="flex flex-col">
+            <p className="font-bold">Change team visibility</p>
+            <p>This team is currently {isPrivate ? "private" : "public"}.</p>
+          </div>
+          {isPrivate ? (
+            <button>Make public</button>
+          ) : (
+            <button>Make private</button>
+          )}
+        </div>
       </div>
     </div>
   );

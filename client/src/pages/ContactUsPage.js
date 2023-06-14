fcc8fd4079c7693bd32245cffa34473b53a0bdaf
vincentochoa3 <vochoa893@gmail.com>
2023-06-14 12:32:24 -0700

    more ui updates and add state for inputs

--- client/src/pages/ContactUsPage.js
+++ client/src/pages/ContactUsPage.js
@@ -1,30 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../components/Logo";
 import FormField from "../components/FormField";
 import ReactQuill from "react-quill";
 import { commentModules } from "../utils/quillModules";
 
 const ContactUsPage = () => {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="flex flex-col mx-auto p-6 items-center max-w-sm sm:p-0">
       <Logo />
       <h1 className="text-4xl text-center text-slate-600 mb-10">Contact Us</h1>
       <h3 className="text-slate-400 text-center mb-8">
-        Please enter the email you signed up with so we can send you a link to
-        reset your password.
+        Let us know if you have any questions or feedback.
       </h3>
-      <form onSubmit={""} className="w-full max-w-sm mb-10">
+      <form onSubmit={handleSubmit} className="w-full max-w-sm mb-10">
         <FormField
           label="Email"
-          id="email-username"
-          type="text"
-          placeholder={"Enter email..."}
-          value={""}
-          onChange={""}
+          id="email"
+          type="email"
+          placeholder={"Enter your email..."}
+          value={email}
+          onChange={(e) => setEmail(e.target.valu)}
         />
+        <label className="block text-sm font-bold mb-2 text-headingColor">
+          Message
+        </label>
         <ReactQuill
-          value={""}
-          onChange={""}
+          id="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
           modules={commentModules}
           theme="snow"
         />

bb129cc2add2c9a6ab528d5312d5556c1d3608a9
Gino Ochoa <gochoa1088@gmail.com>
2023-06-14 14:50:23 -0700

    update links controller

--- server/controllers/linksController.js
+++ server/controllers/linksController.js
@@ -2,6 +2,11 @@ const Link = require("../models/Link");
 
 const addLink = async (req, res, next) => {
   try {
+    if (!req.body) {
+      const error = new Error("Link content required.");
+      error.status = 400;
+      return next(error);
+    }
     const link = await Link.addLinks(req.body);
     res.status(201).json(link);
   } catch (error) {
@@ -13,6 +18,11 @@ const deleteLink = async (req, res, next) => {
   try {
     const { linkId } = req.params;
     const deletedLink = await Link.deleteLink(linkId);
+    if (!deletedLink) {
+      const error = new Error("Link not found.");
+      error.status = 404;
+      return next(error);
+    }
     res.status(200).json({ message: "Link deleted.", deletedLink });
   } catch (error) {
     next(error);

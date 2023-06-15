e1a0a830d38f6448fc1751b0263f110cb9811eeb
Gino Ochoa <gochoa1088@gmail.com>
2023-06-14 22:19:55 -0700

    google cloud setup

---
+++ server/utils/googleAuth.js
@@ -0,0 +1,10 @@
+const { OAuth2Client } = require("google-auth-library");
+const jwt = require("jsonwebtoken");
+
+/*
+ * STEP 1 - Setup Google Cloud
+ * A - Create new project
+ * B - Setup OAuth consent screen
+ * C - Setup credentials and OAuth Client IDs
+ * D - Save client IDs to env
+ */

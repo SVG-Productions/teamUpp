b669c6d09065764e5af68f18bbc01f595a3dcfb5
Gino Ochoa <gochoa1088@gmail.com>
2023-06-27 14:50:32 -0700

    unauthed layout and loading spinner

--- client/src/components/LoadingSpinner.tsx
+++ client/src/components/LoadingSpinner.tsx
@@ -1,4 +1,6 @@
-const LoadingSpinner = ({ message }) => {
+import React from "react";
+
+const LoadingSpinner = ({ message }: { message: string }) => {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex flex-col items-center justify-center z-50 backdrop-blur-sm">
       <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-gray-900"></div>

014c7ec6ef4602c5db2f30a28f90884e0daaf599
vincentochoa3 <vochoa893@gmail.com>
2023-07-11 10:57:20 -0700

    add teamListing image

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -22,70 +22,73 @@ const Landing = () => {
           </Link>
         </div>
       </div>
-      <div className="flex flex-col w-full items-center min-h-[calc(100vh-4rem)] p-6 pt-0 mt-16">
-        <div className="flex flex-col items-center max-w-5xl">
+      <div className="flex flex-col items-center min-h-[calc(100vh-4rem)] p-6 pt-0 mt-16 max-w-5xl">
+        <div className="flex flex-col items-center justify-between sm:flex-row-reverse">
           <Logo />
-          <h1 className="font-semibold">Welcome to TeamApp!</h1>
-          <p className="mt-4">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis
-            est sequi repellendus asperiores earum esse laborum? Molestias quam
-            voluptatum sunt libero itaque quidem doloribus, nulla, earum soluta,
-            enim eveniet nesciunt?
-          </p>
-          <div className="w-full mt-8 sm:flex">
-            <div>
-              <h3 className="font-semibold">
-                Keep track of your job applications.
-              </h3>
-              <p className="py-2 px-6 text-center sm:py-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
-                labore dolores perspiciatis tempora eum amet facilis
-                voluptatibus! Aliquam nesciunt mollitia ipsa nihil! Libero
-                ratione nobis cupiditate in accusamus molestias nesciunt.
-              </p>
-            </div>
-            <img className="rounded-md sm:w-1/2" src="/landing/favorites.png" />
+          <div className="sm:w-3/4">
+            <h1 className="font-semibold">Welcome to TeamApp!</h1>
+            <p className="mt-4">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis
+              est sequi repellendus asperiores earum esse laborum? Molestias
+              quam voluptatum sunt libero itaque quidem doloribus, nulla, earum
+              soluta, enim eveniet nesciunt?
+            </p>
           </div>
-          <div className="w-full mt-8 sm:flex sm:flex-row-reverse">
-            <div>
-              <h3 className="font-semibold text-right">
-                Share with your peers.
-              </h3>
-              <p className="py-4 px-6 text-center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
-                labore dolores perspiciatis tempora eum amet facilis
-                voluptatibus! Aliquam nesciunt mollitia ipsa nihil! Libero
-                ratione nobis cupiditate in accusamus molestias nesciunt.
-              </p>
-            </div>
-            <img className="rounded-md sm:w-1/2" src="/landing/teams.png" />
+        </div>
+        <div className="w-full mt-8 sm:flex">
+          <div>
+            <h3 className="font-semibold">
+              Keep track of your job applications.
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+              labore dolores perspiciatis tempora eum amet facilis voluptatibus!
+              Aliquam nesciunt mollitia ipsa nihil! Libero ratione nobis
+              cupiditate in accusamus molestias nesciunt.
+            </p>
           </div>
-          <div className="w-full mt-8 sm:flex">
-            <div>
-              <h3 className="font-semibold">Post and organize job listings.</h3>
-              <p className="py-4 px-6 text-center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
-                labore dolores perspiciatis tempora eum amet facilis
-                voluptatibus! Aliquam nesciunt mollitia ipsa nihil! Libero
-                ratione nobis cupiditate in accusamus molestias nesciunt.
-              </p>
-            </div>
-            <img className="rounded-md sm:w-1/2" src="/landing/teams.png" />
+          <img className="rounded-md sm:w-1/2" src="/landing/favorites.png" />
+        </div>
+        <div className="w-full mt-8 sm:flex sm:flex-row-reverse">
+          <div>
+            <h3 className="font-semibold text-right">Share with your peers.</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+              labore dolores perspiciatis tempora eum amet facilis voluptatibus!
+              Aliquam nesciunt mollitia ipsa nihil! Libero ratione nobis
+              cupiditate in accusamus molestias nesciunt.
+            </p>
           </div>
-          <div className="w-full mt-8 sm:flex sm:flex-row-reverse">
-            <div className="">
-              <h3 className="font-semibold text-right">
-                Document application experiences.
-              </h3>
-              <p className="py-4 px-6 text-center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
-                labore dolores perspiciatis tempora eum amet facilis
-                voluptatibus! Aliquam nesciunt mollitia ipsa nihil! Libero
-                ratione nobis cupiditate in accusamus molestias nesciunt.
-              </p>
-            </div>
-            <img className="rounded-md sm:w-1/2" src="/landing/teams.png" />
+          <img
+            className="rounded-md sm:w-1/2"
+            src="/landing/teamListings.png"
+          />
+        </div>
+        <div className="w-full mt-8 sm:flex">
+          <div>
+            <h3 className="font-semibold">Post and organize job listings.</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+              labore dolores perspiciatis tempora eum amet facilis voluptatibus!
+              Aliquam nesciunt mollitia ipsa nihil! Libero ratione nobis
+              cupiditate in accusamus molestias nesciunt.
+            </p>
           </div>
+          <img className="rounded-md sm:w-1/2" src="/landing/teams.png" />
+        </div>
+        <div className="w-full mt-8 sm:flex sm:flex-row-reverse">
+          <div className="">
+            <h3 className="font-semibold text-right">
+              Document application experiences.
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+              labore dolores perspiciatis tempora eum amet facilis voluptatibus!
+              Aliquam nesciunt mollitia ipsa nihil! Libero ratione nobis
+              cupiditate in accusamus molestias nesciunt.
+            </p>
+          </div>
+          <img className="rounded-md sm:w-1/2" src="/landing/teams.png" />
         </div>
       </div>
     </>

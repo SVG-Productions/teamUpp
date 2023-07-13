06d15f76e44320dc2016cbf61d543aca0460b840
vincentochoa3 <vochoa893@gmail.com>
2023-07-13 16:42:38 -0700

    add teamwork.jpg and more scroll animations to text

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "./Logo";
 import { motion } from "framer-motion";
 
 const Landing = () => {
+  const [isThreeInView, setIsThreeInView] = useState(false);
+  const [isFourInView, setIsFourInView] = useState(false);
   return (
     <>
       <div className="fixed top-0 bg-white flex items-center justify-between gap-3 w-full p-2 shadow-sm">
@@ -37,7 +39,12 @@ const Landing = () => {
           </div>
         </div>
         <div className="w-full mt-8 md:flex">
-          <div className="self-start mb-4 md:w-2/5 md:p-4">
+          <motion.div
+            initial={{ x: -2000 }}
+            animate={{ x: 0 }}
+            transition={{ ease: "easeOut", duration: 0.5, delay: 0.5 }}
+            className="self-start mb-4 md:w-2/5 md:p-4"
+          >
             <h3 className="font-semibold text-xl">
               Keep track of your job applications.
             </h3>
@@ -47,35 +54,53 @@ const Landing = () => {
               Aliquam nesciunt mollitia ipsa nihil! Libero ratione nobis
               cupiditate in accusamus molestias nesciunt.
             </p>
-          </div>
+          </motion.div>
           <motion.img
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ ease: "easeOut", duration: 1 }}
-            className="shadow-md rounded-md md:w-3/5"
+            className="shadow-md rounded-md md:w-3/5 md:h-auto"
             src="/landing/favorites.png"
           />
         </div>
-        <div className="w-full mt-8 md:flex">
+        <div className="w-full mt-8 md:flex md:flex-row-reverse">
+          <motion.div
+            initial={{ x: 2000 }}
+            animate={{ x: 0 }}
+            transition={{ ease: "easeOut", duration: 0.5, delay: 0.5 }}
+            className="justify-end mb-4 md:w-1/2 md:p-4 md:ml-8"
+          >
+            <h3 className="font-semibold text-xl">Share with your peers.</h3>
+            <p>
+              One of our visions here at TeamApp was to create a space that
+              invites people to not only share listings with eachother, but to
+              give your teammates an idea of what the hiring process at
+              companies looks like. Use the "experience" feature to provide
+              yourself and teammates about specific details, such as the
+              interviewer, interview questions, and more.
+            </p>
+          </motion.div>
           <motion.img
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ ease: "easeOut", duration: 1 }}
-            className="shadow-md rounded-md md:w-3/5"
-            src="/landing/teams.png"
+            className="shadow-md rounded-md md:w-1/2 md:h-auto"
+            src="/landing/teamwork.jpg"
           />
-          <div className="justify-end mb-4 md:w-2/5 md:p-4 md:ml-8">
-            <h3 className="font-semibold text-xl">Share with your peers.</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
-              labore dolores perspiciatis tempora eum amet facilis voluptatibus!
-              Aliquam nesciunt mollitia ipsa nihil! Libero ratione nobis
-              cupiditate in accusamus molestias nesciunt.
-            </p>
-          </div>
         </div>
-        <div className="w-full mt-8 md:flex">
-          <div className="self-start mb-4 md:p-4 md:mr-8">
+        <motion.div
+          whileInView={() => {
+            setIsThreeInView(true);
+            return {};
+          }}
+          className="w-full mt-8 md:flex"
+        >
+          <motion.div
+            initial={{ x: -2000 }}
+            animate={isThreeInView && { x: 0 }}
+            transition={{ delay: 0.5, duration: 0.5 }}
+            className="self-start mb-4 md:p-4 md:mr-8"
+          >
             <h3 className="font-semibold text-xl">
               Post and organize job listings.
             </h3>
@@ -85,24 +110,29 @@ const Landing = () => {
               Aliquam nesciunt mollitia ipsa nihil! Libero ratione nobis
               cupiditate in accusamus molestias nesciunt.
             </p>
-          </div>
+          </motion.div>
           <motion.img
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
             transition={{ ease: "easeOut", duration: 1 }}
             className="shadow-md rounded-md md:w-3/5"
             src="/landing/teamListings.png"
           />
-        </div>
-        <div className="w-full mt-8 md:flex">
-          <motion.img
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            transition={{ ease: "easeOut", duration: 1 }}
-            className="shadow-md rounded-md md:w-3/5"
-            src="/landing/experience.png"
-          />
-          <div className="self-start mb-4 md:p-4 md:ml-8">
+        </motion.div>
+        <motion.div
+          whileInView={() => {
+            setIsFourInView(true);
+            return {};
+          }}
+          className="w-full mt-8 md:flex md:flex-row-reverse"
+        >
+          <motion.div
+            initial={{ x: 2000 }}
+            animate={isFourInView && { x: 0 }}
+            transition={{ delay: 0.5, duration: 0.5 }}
+            className="self-start mb-4 md:p-4 md:ml-8"
+          >
             <h3 className="font-semibold text-xl">
               Document application experiences.
             </h3>
@@ -112,8 +142,16 @@ const Landing = () => {
               Aliquam nesciunt mollitia ipsa nihil! Libero ratione nobis
               cupiditate in accusamus molestias nesciunt.
             </p>
-          </div>
-        </div>
+          </motion.div>
+          <motion.img
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ ease: "easeOut", duration: 1 }}
+            className="shadow-md rounded-md md:w-3/5"
+            src="/landing/experience.png"
+          />
+        </motion.div>
       </div>
     </>
   );

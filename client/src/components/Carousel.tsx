0b4f5208280962547f41a50cdaefd67258f727d3
vincentochoa3 <vochoa893@gmail.com>
2023-07-18 11:12:35 -0700

    render images and button fucntionality

--- client/src/components/Carousel.tsx
+++ client/src/components/Carousel.tsx
@@ -1,11 +1,34 @@
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 
 const Carousel = ({ images }: { images: string[] }) => {
-  const [currentIndex, setCurrentIndex] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  console.log(images);
+  const handleNext = () => {
+    setCurrentIndex((prev) => (prev + 1 === images.length ? 0 : prev + 1));
+  };
 
-  return <div>Carousel</div>;
+  const handlePrevious = () => {
+    setCurrentIndex((prev) => (prev - 1 < 0 ? images.length - 1 : prev - 1));
+  };
+
+  return (
+    <div>
+      <img src={images[currentIndex]} />
+      <div>
+        <button onClick={handlePrevious}>
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </button>
+        <button onClick={handleNext}>
+          <FontAwesomeIcon icon={faChevronRight} />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Carousel;

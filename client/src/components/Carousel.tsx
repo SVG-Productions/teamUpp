77756c0bdc1760094dcd5b23c385cde570398eca
vincentochoa3 <vochoa893@gmail.com>
2023-07-18 14:22:43 -0700

    add variant and slide animation for carousel images

--- client/src/components/Carousel.tsx
+++ client/src/components/Carousel.tsx
@@ -1,29 +1,69 @@
+import React, { useState } from "react";
 import {
   faChevronLeft,
   faChevronRight,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Carousel = ({ images }: { images: string[] }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [direction, setDirection] = useState("left");
+
+  const slideVariants = {
+    hiddenRight: {
+      x: "100%",
+      opacity: 0,
+    },
+    hiddenLeft: {
+      x: "-100%",
+      opacity: 0,
+    },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: { duration: 0.3, delay: 0.2 },
+    },
+    // exitLeft: {
+    //   x: "100%",
+    //   opacity: 0,
+    //   transition: { duration: 0.1 },
+    // },
+    // exitRight: {
+    //   x: "-100%",
+    //   opacity: 0,
+    //   transition: { duration: 0.1 },
+    // },
+  };
 
   const handleNext = () => {
+    setDirection("right");
     setCurrentIndex((prev) => (prev + 1 === images.length ? 0 : prev + 1));
   };
 
   const handlePrevious = () => {
+    setDirection("left");
     setCurrentIndex((prev) => (prev - 1 < 0 ? images.length - 1 : prev - 1));
   };
 
   const handleDotClick = (index: number) => {
+    setDirection(index > currentIndex ? "right" : "left");
     setCurrentIndex(index);
   };
 
   return (
     <>
-      <div className="relative rounded-md shadow-md">
-        <img className="rounded-md" src={images[currentIndex]} />
+      <div className="relative rounded-md">
+        <AnimatePresence>
+          <motion.img
+            key={currentIndex}
+            className="rounded-md shadow-md"
+            src={images[currentIndex]}
+            variants={slideVariants}
+            initial={direction === "right" ? "hiddenRight" : "hiddenLeft"}
+            animate="visible"
+          />
+        </AnimatePresence>
         <div className="absolute top-0 bottom-0 flex justify-between items-center w-full">
           <button
             onClick={handlePrevious}

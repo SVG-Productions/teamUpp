3d39707ebd4674efe99c65cc72ed3fd785ea57db
vincentochoa3 <vochoa893@gmail.com>
2023-07-18 12:19:23 -0700

    add sytling to carousel and dot indicator buttons

--- client/src/components/Carousel.tsx
+++ client/src/components/Carousel.tsx
@@ -16,18 +16,41 @@ const Carousel = ({ images }: { images: string[] }) => {
     setCurrentIndex((prev) => (prev - 1 < 0 ? images.length - 1 : prev - 1));
   };
 
+  const handleDotClick = (index: number) => {
+    setCurrentIndex(index);
+  };
+
   return (
-    <div>
-      <img src={images[currentIndex]} />
-      <div>
-        <button onClick={handlePrevious}>
-          <FontAwesomeIcon icon={faChevronLeft} />
-        </button>
-        <button onClick={handleNext}>
-          <FontAwesomeIcon icon={faChevronRight} />
-        </button>
+    <>
+      <div className="relative rounded-md">
+        <img className="max-h-64 rounded-md" src={images[currentIndex]} />
+        <div className="absolute top-0 bottom-0 flex justify-between items-center w-full">
+          <button
+            onClick={handlePrevious}
+            className="mx-4 opacity-50 w-12 h-12 rounded-full bg-blue-100"
+          >
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </button>
+          <button
+            onClick={handleNext}
+            className="mx-4 opacity-50 w-12 h-12 rounded-full bg-blue-100"
+          >
+            <FontAwesomeIcon icon={faChevronRight} />
+          </button>
+        </div>
       </div>
-    </div>
+      <div className="flex justify-center items-center mt-4 gap-2 h-10">
+        {images.map((image, index) => (
+          <button
+            key={image}
+            onClick={() => handleDotClick(index)}
+            className={`rounded-full bg-blue-500 ${
+              index === currentIndex ? "w-5 h-5" : "w-4 h-4"
+            }`}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 

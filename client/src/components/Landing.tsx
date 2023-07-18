3446636a7061e4e33196e3b2c4be37b35d46e6e4
vincentochoa3 <vochoa893@gmail.com>
2023-07-18 09:57:07 -0700

    add third stock image and description

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -79,14 +79,38 @@ const Landing = () => {
             transition={{ duration: 0.5 }}
             className="mb-4 md:p-4 md:ml-8"
           >
+            <h3 className="font-semibold text-3xl">
+              Document your application experiences.
+            </h3>
+            <p className="text-lg leading-8">
+              Keep track of your progress using the Experiences feature. Your
+              "experience" can be used to provide yourself and teammates
+              specific details, such as the interviewer, interview questions,
+              and more.
+            </p>
+          </motion.div>
+          <motion.img
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ ease: "easeOut", duration: 1.4 }}
+            className="shadow-md rounded-lg md:w-2/5 md:h-auto"
+            src="/landing/interview.jpg"
+          />
+        </div>
+        <div className="w-full mt-8 md:flex md:items-center">
+          <motion.div
+            initial={{ y: 50, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="mb-4 md:p-4 md:ml-8"
+          >
             <h3 className="font-semibold text-3xl">Share with your peers.</h3>
             <p className="text-lg leading-8">
               One of our visions here at TeamApp was to create a space that
               invites people to not only share listings with eachother, but to
               give your teammates an idea of what the hiring process at
-              companies looks like. Use the "experience" feature to provide
-              yourself and teammates about specific details, such as the
-              interviewer, interview questions, and more.
+              companies looks like.
             </p>
           </motion.div>
           <motion.img
@@ -98,58 +122,6 @@ const Landing = () => {
             src="/landing/teamwork.jpg"
           />
         </div>
-        <div className="w-full mt-8 md:flex md:items-center">
-          <motion.div
-            initial={{ y: 50, opacity: 0 }}
-            whileInView={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.5 }}
-            className="mb-4 md:p-4 md:mr-8"
-          >
-            <h3 className="font-semibold text-xl">
-              Post and organize job listings.
-            </h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
-              labore dolores perspiciatis tempora eum amet facilis voluptatibus!
-              Aliquam nesciunt mollitia ipsa nihil! Libero ratione nobis
-              cupiditate in accusamus molestias nesciunt.
-            </p>
-          </motion.div>
-          <motion.img
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            viewport={{ once: true }}
-            transition={{ ease: "easeOut", duration: 1.4 }}
-            className="shadow-md rounded-md md:w-3/5"
-            src="/landing/teamListings.png"
-          />
-        </div>
-        <motion.div className="w-full mt-8 md:flex md:flex-row-reverse md:items-center">
-          <motion.div
-            initial={{ y: 50, opacity: 0 }}
-            whileInView={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.5 }}
-            className="mb-4 md:p-4 md:ml-8"
-          >
-            <h3 className="font-semibold text-xl">
-              Document application experiences.
-            </h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
-              labore dolores perspiciatis tempora eum amet facilis voluptatibus!
-              Aliquam nesciunt mollitia ipsa nihil! Libero ratione nobis
-              cupiditate in accusamus molestias nesciunt.
-            </p>
-          </motion.div>
-          <motion.img
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            viewport={{ once: true }}
-            transition={{ ease: "easeOut", duration: 1.4 }}
-            className="shadow-md rounded-md md:w-3/5"
-            src="/landing/experience.png"
-          />
-        </motion.div>
       </div>
     </div>
   );

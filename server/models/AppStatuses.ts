e3e1b18eae711862480924ecf4ba6564af808238
Gino Ochoa <gochoa1088@gmail.com>
2023-07-24 16:04:44 -0700

    apply delete -> insert strategy for columns

--- server/models/AppStatuses.ts
+++ server/models/AppStatuses.ts
@@ -2,13 +2,14 @@ const knex = require("../dbConfig");
 
 const updateUserAppStatuses = async (userId: string, statusOrder: string[]) => {
   try {
+    await knex("application_statuses").where("user_id", userId).del();
     console.log(userId, statusOrder);
     statusOrder.forEach(async (s, i) => {
       console.log(s, i);
       await knex("application_statuses")
         .where("user_id", userId)
         .andWhere("app_status", s)
-        .update({ index: i });
+        .insert({ userId, appStatus: s, index: i });
     });
   } catch (error: any) {
     console.error("Database Error: " + error.message);

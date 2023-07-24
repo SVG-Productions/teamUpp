fce4196e5a16b7e4c93c74e769b1da2e8335c926
Gino Ochoa <gochoa1088@gmail.com>
2023-07-24 16:46:42 -0700

    change back to iterating update

--- server/models/AppStatuses.ts
+++ server/models/AppStatuses.ts
@@ -2,12 +2,11 @@ const knex = require("../dbConfig");
 
 const updateUserAppStatuses = async (userId: string, statusOrder: string[]) => {
   try {
-    await knex("application_statuses").where("user_id", userId).del();
     statusOrder.forEach(async (s, i) => {
       await knex("application_statuses")
         .where("user_id", userId)
         .andWhere("app_status", s)
-        .insert({ userId, appStatus: s, index: i });
+        .update({ index: i });
     });
   } catch (error: any) {
     console.error("Database Error: " + error.message);

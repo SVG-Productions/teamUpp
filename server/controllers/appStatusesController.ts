75fe55b26f255bbd59e7d4adfadb824185323e9a
Gino Ochoa <gochoa1088@gmail.com>
2023-07-24 15:46:22 -0700

    create appStatusesController

---
+++ server/controllers/appStatusesController.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+const AppStatuses = require("../models/AppStatuses");
+
+const updateUserAppStatuses = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const id = req.user?.id;
+    const statusOrder = req.body;
+    await UpdateUserAppStatuses.updateUserAppStatuses(id, statusOrder);
+    res.status(200).json({ message: "App statuses successfully updated." });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  updateUserAppStatuses,
+};
+
+export {};

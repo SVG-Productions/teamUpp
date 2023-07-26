de98515580028746ea58e2826b8b25743dade915
Gino Ochoa <gochoa1088@gmail.com>
2023-07-26 12:06:47 -0700

    add edit status route

--- server/routes/appStatuses.ts
+++ server/routes/appStatuses.ts
@@ -5,9 +5,11 @@ const { requireAuth } = require("../utils/auth");
 const {
   updateUserAppStatuses,
   addUserAppStatus,
+  editUserAppStatus,
 } = require("../controllers/appStatusesController");
 
 router.post("/", requireAuth, addUserAppStatus);
+router.patch("/", requireAuth, editUserAppStatus);
 router.patch("/status-order", requireAuth, updateUserAppStatuses);
 
 module.exports = router;

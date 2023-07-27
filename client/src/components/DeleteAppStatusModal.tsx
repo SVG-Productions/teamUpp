b18e6d0e2b0910cbc50f46bf0a69b0aa9eb7be19
Gino Ochoa <gochoa1088@gmail.com>
2023-07-27 12:59:37 -0700

    write containers for status to be removed and status to be moved to

--- client/src/components/DeleteAppStatusModal.tsx
+++ client/src/components/DeleteAppStatusModal.tsx
@@ -16,13 +16,27 @@ const DeleteAppStatusModal = ({
             className="mr-4 text-red-600"
             icon={faExclamationTriangle}
           />
-          Move applications from the column.
+          Move applications from the column
         </h2>
-        <p className="text-sm">
+        <p className="text-sm text-tertiary">
           You're about to permanently delete this application status. This is
           irreversible. In order to preserve your applications, please select
           another status to move your applications to.
         </p>
+        <div className="flex justify-between mt-6">
+          <div className="flex flex-col">
+            <p className="font-semibold text-secondary">
+              This status will be deleted:
+            </p>
+          </div>
+          <div className="flex">
+            <div className="flex flex-col">
+              <p className="font-semibold text-secondary">
+                Move existing applications to:
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </ModalLayout>
   );

c2469294aa5cbef9edb6417d246041eea380f9cb
Gino Ochoa <gochoa1088@gmail.com>
2023-08-01 12:59:13 -0700

    setup modal state

--- client/src/components/AppItem.tsx
+++ client/src/components/AppItem.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Draggable } from "react-beautiful-dnd";
 
 const AppItem = ({ task, index }: { task: any; index: number }) => {
+  const [showAppModal, setShowAppModal] = useState(false);
+  console.log(task);
   return (
     <Draggable draggableId={task.id} index={index}>
       {(provided, snapshot) => (
@@ -10,6 +12,7 @@ const AppItem = ({ task, index }: { task: any; index: number }) => {
           className={`p-2 mb-1 rounded-sm bg-primary ${
             snapshot.isDragging && "bg-tertiary"
           }`}
+          onClick={() => setShowAppModal(true)}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >

95ac6fcef44bf6ec933fad453260e30146acd927
Gino Ochoa <gochoa1088@gmail.com>
2023-08-01 13:57:15 -0700

    add loading state to useEffect

--- client/src/components/BoardAppDetailsModal.tsx
+++ client/src/components/BoardAppDetailsModal.tsx
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
 import ModalLayout from "../layouts/ModalLayout";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEllipsisH,
+  faEllipsisV,
+  faX,
+} from "@fortawesome/free-solid-svg-icons";
+import LoadingSpinner from "./LoadingSpinner";
 
 const BoardAppDetailsModal = ({
   handleModal,
@@ -10,11 +17,13 @@ const BoardAppDetailsModal = ({
   task: any;
 }) => {
   const [taskData, setTaskData] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchListingData = async () => {
       const { data } = await axios.get(`/api/listings/${task.id}`);
       setTaskData(data);
+      setLoading(false);
     };
     fetchListingData();
   }, []);
@@ -23,11 +32,33 @@ const BoardAppDetailsModal = ({
 
   return (
     <ModalLayout handleClickOut={handleModal}>
-      <div className="bg-primary w-full max-w-lg p-5 mx-auto z-10 sm:rounded-md sm:shadow-lg">
-        <h2 className="sm:text-lg font-medium mb-8 sm:mb-4 text-center">
-          {task.companyName} - {task.jobTitle}
-        </h2>
-      </div>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <div className="bg-primary w-full max-w-4xl p-5 mx-auto z-10 sm:rounded-md sm:shadow-lg">
+          <div className="flex justify-between items-center">
+            <h2 className="sm:text-lg font-medium text-center">
+              {task.companyName} - {task.jobTitle}
+              <span className="bg-secondary text-secondary rounded-sm text-sm p-0.5 ml-4 ">
+                {taskData.appStatus}
+              </span>
+            </h2>
+            <div className="flex items-center gap-5">
+              <FontAwesomeIcon
+                size="lg"
+                icon={faEllipsisH}
+                className="rounded-sm hover:text-secondary"
+              />
+              <FontAwesomeIcon
+                size="lg"
+                icon={faX}
+                className="rounded-sm hover:text-secondary"
+                onClick={() => handleModal(false)}
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </ModalLayout>
   );
 };

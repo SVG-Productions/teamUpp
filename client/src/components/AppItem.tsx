5b691cf30d954d9a15b0654ac84381f9067a5707
Gino Ochoa <gochoa1088@gmail.com>
2023-08-01 13:03:04 -0700

    basic layout and title

--- client/src/components/AppItem.tsx
+++ client/src/components/AppItem.tsx
@@ -1,25 +1,37 @@
 import React, { useState } from "react";
 import { Draggable } from "react-beautiful-dnd";
+import ModalLayout from "../layouts/ModalLayout";
 
 const AppItem = ({ task, index }: { task: any; index: number }) => {
   const [showAppModal, setShowAppModal] = useState(false);
   console.log(task);
   return (
-    <Draggable draggableId={task.id} index={index}>
-      {(provided, snapshot) => (
-        <div
-          ref={provided.innerRef}
-          className={`p-2 mb-1 rounded-sm bg-primary ${
-            snapshot.isDragging && "bg-tertiary"
-          }`}
-          onClick={() => setShowAppModal(true)}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-        >
-          {task.companyName + " - " + task.jobTitle}
-        </div>
+    <>
+      {showAppModal && (
+        <ModalLayout handleClickOut={setShowAppModal}>
+          <div className="bg-primary w-full max-w-lg p-5 mx-auto z-10 sm:rounded-md sm:shadow-lg">
+            <h2 className="sm:text-lg font-medium mb-8 sm:mb-4 text-center">
+              {task.companyName} - {task.jobTitle}
+            </h2>
+          </div>
+        </ModalLayout>
       )}
-    </Draggable>
+      <Draggable draggableId={task.id} index={index}>
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            className={`p-2 mb-1 rounded-sm bg-primary ${
+              snapshot.isDragging && "bg-tertiary"
+            }`}
+            onClick={() => setShowAppModal(true)}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+          >
+            {task.companyName + " - " + task.jobTitle}
+          </div>
+        )}
+      </Draggable>
+    </>
   );
 };
 

370a6cfb1e699fe677fa69bb780b7718433d3c75
Gino Ochoa <gochoa1088@gmail.com>
2023-08-03 16:14:51 -0700

    create board context

---
+++ client/src/context/BoardContext.tsx
@@ -0,0 +1,21 @@
+import React, { useState, createContext, useContext } from "react";
+import { useRouteLoaderData } from "react-router-dom";
+import { UserType } from "../../type-definitions";
+
+const BoardContext = createContext<any>({});
+
+export const useBoard = () => useContext(BoardContext);
+export const BoardProvider = ({ children }: { children: any }) => {
+  const { userData } = useRouteLoaderData("apps") as {
+    userData: UserType;
+  };
+  const [boardData, setBoardData] = useState<any>(
+    userData.applications.boardApps
+  );
+
+  return (
+    <BoardContext.Provider value={{ boardData, setBoardData }}>
+      {children}
+    </BoardContext.Provider>
+  );
+};

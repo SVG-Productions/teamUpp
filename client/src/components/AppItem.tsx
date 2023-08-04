c77ce9e74bc2c5965c3076df5062a7ae38362732
Gino Ochoa <gochoa1088@gmail.com>
2023-08-04 14:07:07 -0700

    cleanup AppItem

--- client/src/components/AppItem.tsx
+++ client/src/components/AppItem.tsx
@@ -1,36 +1,29 @@
-import React, { useState, memo } from "react";
+import React, { memo } from "react";
 import { Draggable } from "react-beautiful-dnd";
-import BoardAppDetailsModal from "./BoardAppDetailsModal";
 import { useBoard } from "../context/BoardContext";
 
 const AppItem = ({ task, index }: { task: any; index: number }) => {
-  // const [showAppModal, setShowAppModal] = useState(false);
   const { setShowAppDetails, setSelectedApp } = useBoard();
 
   return (
-    <>
-      {/* {showAppModal && (
-        <BoardAppDetailsModal handleModal={setShowAppModal} task={task} />
-      )} */}
-      <Draggable draggableId={task.id} index={index}>
-        {(provided, snapshot) => (
-          <div
-            ref={provided.innerRef}
-            className={`p-2 mb-1 rounded-sm bg-primary ${
-              snapshot.isDragging && "bg-tertiary"
-            }`}
-            onClick={() => {
-              setSelectedApp(task.id);
-              setShowAppDetails(true);
-            }}
-            {...provided.draggableProps}
-            {...provided.dragHandleProps}
-          >
-            {task.companyName + " - " + task.jobTitle}
-          </div>
-        )}
-      </Draggable>
-    </>
+    <Draggable draggableId={task.id} index={index}>
+      {(provided, snapshot) => (
+        <div
+          ref={provided.innerRef}
+          className={`p-2 mb-1 rounded-sm bg-primary ${
+            snapshot.isDragging && "bg-tertiary"
+          }`}
+          onClick={() => {
+            setSelectedApp(task.id);
+            setShowAppDetails(true);
+          }}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          {task.companyName + " - " + task.jobTitle}
+        </div>
+      )}
+    </Draggable>
   );
 };
 

b7019fcab902b39bf364d4ccdb36a311a7cd8e78
Gino Ochoa <gochoa1088@gmail.com>
2023-08-04 16:44:59 -0700

    change styling of AppItem

--- client/src/components/AppItem.tsx
+++ client/src/components/AppItem.tsx
@@ -1,16 +1,16 @@
 import React, { memo } from "react";
 import { Draggable } from "react-beautiful-dnd";
 import { useBoard } from "../context/BoardContext";
+import { formatGeneralDate } from "../utils/dateFormatters";
 
 const AppItem = ({ task, index }: { task: any; index: number }) => {
   const { setShowAppDetails, setSelectedApp } = useBoard();
-
   return (
     <Draggable draggableId={task.id} index={index}>
       {(provided, snapshot) => (
         <div
           ref={provided.innerRef}
-          className={`p-2 mb-1 rounded-sm bg-primary ${
+          className={`flex flex-col p-2 mb-1 rounded-sm bg-primary ${
             snapshot.isDragging && "bg-tertiary"
           }`}
           onClick={() => {
@@ -20,7 +20,13 @@ const AppItem = ({ task, index }: { task: any; index: number }) => {
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >
-          {task.companyName + " - " + task.jobTitle}
+          <div className="flex flex-col mb-2">
+            <p className="text-sm font-semibold truncate">{task.jobTitle}</p>
+            <p className="text-secondary text-xs"> {task.companyName}</p>
+          </div>
+          <p className="text-xs text-tertiary font-extralight self-end">
+            Applied {formatGeneralDate(task.createdAt)}
+          </p>
         </div>
       )}
     </Draggable>

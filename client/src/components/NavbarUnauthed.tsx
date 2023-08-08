e743905f96e62b95b87206674629b9ec7607bc0a
vincentochoa3 <vochoa893@gmail.com>
2023-08-08 14:28:38 -0700

    add uniform animations to unauthed navbar

--- client/src/components/NavbarUnauthed.tsx
+++ client/src/components/NavbarUnauthed.tsx
@@ -1,24 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const NavbarUnauthed = () => {
   return (
     <div className="fixed z-10 top-0 bg-white flex items-center justify-between gap-3 w-full h-16 p-2 shadow-sm">
       <Link to="/">
-        <div className="flex items-center justify-center w-10 h-10 bg-slate-900 rounded-full hover:bg-slate-500">
+        <motion.div
+          whileHover={{ scale: 1.1 }}
+          className="flex items-center justify-center w-10 h-10 bg-slate-900 rounded-full hover:bg-slate-500"
+        >
           <p className="text-white text-xs">t / a</p>
-        </div>
+        </motion.div>
       </Link>
       <div className="flex gap-3 items-center">
-        <Link to="/login" className="font-lg font-semibold text-black">
-          Login
+        <Link
+          to="/login"
+          className="font-lg font-semibold text-black hover:text-slate-500 hover:no-underline"
+        >
+          <motion.div whileHover={{ scale: 1.1 }}>Login</motion.div>
         </Link>
         <Link
           to="/signup"
-          className="font-semibold text-sm min-w-fit py-2 px-4 rounded-md
-          border-2 border-blue-400 hover:bg-blue-100 hover:no-underline md:text-base"
+          className="min-w-fit rounded-md
+          border-2 border-blue-400 hover:bg-blue-100 hover:no-underline"
         >
-          Sign Up
+          <motion.div
+            whileHover={{ scale: 1.1 }}
+            className="py-2 px-4 font-semibold text-sm md:text-base"
+          >
+            Sign Up
+          </motion.div>
         </Link>
       </div>
     </div>

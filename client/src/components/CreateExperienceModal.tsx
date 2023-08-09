48b324d2784966feb4ef41a03414d4788e306786
vincentochoa3 <vochoa893@gmail.com>
2023-08-08 17:26:59 -0700

    change formatting for add experience modal

--- client/src/components/CreateExperienceModal.tsx
+++ client/src/components/CreateExperienceModal.tsx
@@ -106,7 +106,7 @@ const CreateExperienceModal = ({
   return (
     <ModalLayout handleClickOut={handleModal}>
       <div
-        className="relative flex flex-col bg-primary h-full w-full max-w-3xl rounded-sm z-10 
+        className="relative flex flex-col bg-primary h-full w-full max-w-5xl rounded-sm z-10 
         sm:h-fit sm:shadow-lg sm:rounded-md sm:overflow-auto sm:max-h-[90%]"
       >
         <div className="hidden sm:flex sm:absolute sm:right-1 sm:top-1">
@@ -122,122 +122,128 @@ const CreateExperienceModal = ({
         </h2>
         <form
           onSubmit={handleSubmit}
-          className="w-full p-6 sm:max-w-2xl sm:self-center"
+          className="w-full p-6 sm:self-center sm:px-12"
         >
-          <div className="sm:w-2/3">
-            <FormField
-              label="Title"
-              id="title"
-              type="text"
-              placeholder="Enter experience title..."
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </div>
-          <div className="flex flex-col mb-4 sm:w-[95%]">
-            <label
-              htmlFor="content"
-              className="block font-bold text-headingColor mb-2 text-sm"
-            >
-              Description
-            </label>
-            <ReactQuill
-              id="content"
-              value={content}
-              onChange={setContent}
-              modules={basicModules}
-              theme="snow"
-            />
-          </div>
-          <div className="flex flex-col gap-4 mb-4">
-            <div className="flex justify-between items-center">
-              <label
-                htmlFor="content"
-                className="block font-bold text-headingColor text-sm"
-              >
-                Interview questions
-              </label>
-              <FontAwesomeIcon
-                icon={faPlusCircle}
-                size="xl"
-                onClick={addQuestionInput}
-                className="cursor-pointer text-iconPrimary hover:text-iconSecondary"
-              />
-            </div>
-            <ul className="flex flex-col gap-2">
-              {questions.map((question, index) => (
-                <li
-                  className="flex gap-2 items-center sm:w-[95%]"
-                  key={"question" + index}
+          <div className="flex flex-col sm:gap-8 sm:flex-row">
+            <div className="sm:w-1/2">
+              <div className="sm:w-11/12">
+                <FormField
+                  label="Title"
+                  id="title"
+                  type="text"
+                  placeholder="Enter experience title..."
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+              <div className="flex flex-col mb-4 sm:w-[95%]">
+                <label
+                  htmlFor="content"
+                  className="block font-bold text-headingColor mb-2 text-sm"
                 >
-                  <input
-                    className="border border-borderprimary rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-bluegray"
-                    type="text"
-                    value={question}
-                    required
-                    onChange={(event) => handleQuestionChange(index, event)}
-                    placeholder="Enter question... "
-                  />
-                  <FontAwesomeIcon
-                    icon={faTrashCan}
-                    className="cursor-pointer text-iconPrimary hover:text-red-500 ml-2"
-                    size="xl"
-                    onClick={() => deleteQuestion(index)}
-                  />
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="flex flex-col gap-4">
-            <div className="flex justify-between items-center">
-              <label
-                htmlFor="content"
-                className="block font-bold text-headingColor text-sm"
-              >
-                Helpful links
-              </label>
-              <FontAwesomeIcon
-                icon={faPlusCircle}
-                size="xl"
-                onClick={addLinkInput}
-                className="cursor-pointer text-iconPrimary hover:text-iconSecondary"
-              />
+                  Description
+                </label>
+                <ReactQuill
+                  id="content"
+                  value={content}
+                  onChange={setContent}
+                  modules={basicModules}
+                  theme="snow"
+                />
+              </div>
             </div>
-            <ul className="flex flex-col gap-4 sm:gap-2">
-              {links.map((link, index) => (
-                <li
-                  className="flex items-center border-2 rounded-md sm:border-none sm:w-[95%] sm:gap-2"
-                  key={"link" + index}
-                >
-                  <div className="flex flex-col w-full gap-2 p-2 pl-0 sm:flex-row sm:p-0">
-                    <input
-                      className="border border-borderprimary rounded py-2 px-3 text-gray-700 leading-tight focus:outline-bluegray sm:w-2/5"
-                      type="text"
-                      value={link.description}
-                      onChange={(event) =>
-                        handleLinkChange(index, "description", event)
-                      }
-                      placeholder="Link description... "
-                    />
-                    <input
-                      className="border border-borderprimary rounded py-2 px-3 text-gray-700 leading-tight focus:outline-bluegray sm:w-3/5"
-                      type="url"
-                      value={link.url}
-                      onChange={(event) =>
-                        handleLinkChange(index, "url", event)
-                      }
-                      placeholder="Enter url..."
-                    />
-                  </div>
+            <div className="sm:w-1/2 sm:border sm:border-borderprimary sm:rounded-md sm:p-4 sm:mb-4">
+              <div className="flex flex-col gap-4 mb-4">
+                <div className="flex justify-between items-center">
+                  <label
+                    htmlFor="content"
+                    className="block font-bold text-headingColor text-sm"
+                  >
+                    Interview questions
+                  </label>
                   <FontAwesomeIcon
-                    icon={faTrashCan}
-                    className="cursor-pointer text-iconPrimary hover:text-red-500 ml-2"
+                    icon={faPlusCircle}
                     size="xl"
-                    onClick={() => deleteLink(index)}
+                    onClick={addQuestionInput}
+                    className="cursor-pointer text-iconPrimary hover:text-iconSecondary"
                   />
-                </li>
-              ))}
-            </ul>
+                </div>
+                <ul className="flex flex-col gap-2">
+                  {questions.map((question, index) => (
+                    <li
+                      className="flex gap-2 items-center sm:w-[95%]"
+                      key={"question" + index}
+                    >
+                      <input
+                        className="border border-borderprimary rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-bluegray"
+                        type="text"
+                        value={question}
+                        required
+                        onChange={(event) => handleQuestionChange(index, event)}
+                        placeholder="Enter question... "
+                      />
+                      <FontAwesomeIcon
+                        icon={faTrashCan}
+                        className="cursor-pointer text-iconPrimary hover:text-red-500 ml-2"
+                        size="xl"
+                        onClick={() => deleteQuestion(index)}
+                      />
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="flex flex-col gap-4">
+                <div className="flex justify-between items-center">
+                  <label
+                    htmlFor="content"
+                    className="block font-bold text-headingColor text-sm"
+                  >
+                    Helpful links
+                  </label>
+                  <FontAwesomeIcon
+                    icon={faPlusCircle}
+                    size="xl"
+                    onClick={addLinkInput}
+                    className="cursor-pointer text-iconPrimary hover:text-iconSecondary"
+                  />
+                </div>
+                <ul className="flex flex-col gap-4 sm:gap-2">
+                  {links.map((link, index) => (
+                    <li
+                      className="flex items-center border rounded-md px-2 sm:border-none sm:w-[95%] sm:gap-2 sm:px-0"
+                      key={"link" + index}
+                    >
+                      <div className="flex flex-col w-full gap-2 p-2 pl-0 sm:flex-row sm:p-0">
+                        <input
+                          className="border border-borderprimary rounded py-2 px-3 text-gray-700 leading-tight focus:outline-bluegray sm:w-2/5"
+                          type="text"
+                          value={link.description}
+                          onChange={(event) =>
+                            handleLinkChange(index, "description", event)
+                          }
+                          placeholder="Link description... "
+                        />
+                        <input
+                          className="border border-borderprimary rounded py-2 px-3 text-gray-700 leading-tight focus:outline-bluegray sm:w-3/5"
+                          type="url"
+                          value={link.url}
+                          onChange={(event) =>
+                            handleLinkChange(index, "url", event)
+                          }
+                          placeholder="Enter url..."
+                        />
+                      </div>
+                      <FontAwesomeIcon
+                        icon={faTrashCan}
+                        className="cursor-pointer text-iconPrimary hover:text-red-500 ml-2"
+                        size="xl"
+                        onClick={() => deleteLink(index)}
+                      />
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
           </div>
           <div className="flex justify-center mt-8 gap-3">
             <CreateFormButtonGroup handleCancel={() => handleModal(false)} />

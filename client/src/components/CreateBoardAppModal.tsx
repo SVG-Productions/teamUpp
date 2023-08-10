6767085ac25c15e5ca03e5d7f2c8fcb9b8cfd9be
vincentochoa3 <vochoa893@gmail.com>
2023-08-10 11:26:11 -0700

    add wide format for desktop view media queries

--- client/src/components/CreateBoardAppModal.tsx
+++ client/src/components/CreateBoardAppModal.tsx
@@ -74,7 +74,7 @@ const CreateBoardAppModal = ({
   return (
     <ModalLayout handleClickOut={handleModal}>
       <div
-        className="relative flex flex-col bg-primary h-full w-full max-w-xl rounded-sm z-10 
+        className="relative flex flex-col bg-primary h-full w-full max-w-5xl rounded-sm z-10 
           sm:h-fit sm:shadow-lg sm:rounded-md sm:overflow-auto sm:max-h-[90%]"
       >
         <div className="hidden sm:flex sm:absolute sm:right-1 sm:top-1">
@@ -85,110 +85,112 @@ const CreateBoardAppModal = ({
             onClick={() => handleModal(false)}
           />
         </div>
-        <h2 className="text-lg font-bold mb-6 pt-6 text-center sm:mb-2">
-          Create Listing
-        </h2>
         <form
           onSubmit={handleSubmit}
-          className="w-full p-6 sm:max-w-md sm:self-center"
+          className="flex flex-col w-full p-6 sm:self-center sm:px-12 sm:gap-12 sm:flex-row"
         >
-          <FormField
-            label="Job title"
-            id="jobTitle"
-            type="text"
-            placeholder="Enter job title..."
-            value={jobTitle}
-            onChange={(e) => setJobTitle(e.target.value)}
-          />
-          <FormField
-            label="Application link"
-            id="link"
-            type="url"
-            placeholder="Enter link to application..."
-            value={jobLink}
-            onChange={(e) => setJobLink(e.target.value)}
-          />
-          <FormField
-            label="Company name"
-            id="companyName"
-            type="text"
-            placeholder="Enter company name..."
-            value={companyName}
-            onChange={(e) => setCompanyName(e.target.value)}
-          />
-          <FormField
-            label="Location"
-            id="location"
-            type="text"
-            placeholder="Enter location..."
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-          />
-          <div className="flex flex-col mb-4">
-            <label
-              className="block font-bold text-headingColor mb-2 text-sm"
-              htmlFor="salaryAmount"
-            >
-              Salary
-            </label>
-            <div className="flex">
-              <span className="font-bold text-xl self-center mr-1">$</span>
-              <input
-                className="border border-borderprimary rounded w-full py-2 px-3 mr-2 text-primary leading-tight focus:outline-bluegray"
-                id="salaryAmount"
-                placeholder="0"
-                type="number"
-                min="0"
-                max="1000000"
-                value={salaryAmount}
-                onChange={(e) => setSalaryAmount(e.target.value)}
-                autoComplete="off"
-              />
-              <select
-                className="border border-borderprimary text-xs bg-primary rounded w-fit py-1 px-1.5 text-primary leading-tight focus:outline-bluegray"
-                id="salaryFrequency"
-                value={salaryFrequency}
-                onChange={(e) => setSalaryFrequency(e.target.value)}
+          <div className="w-1/2">
+            <h2 className="text-lg font-bold mb-6 sm:mb-2">Create Listing</h2>
+            <FormField
+              label="Job title"
+              id="jobTitle"
+              type="text"
+              placeholder="Enter job title..."
+              value={jobTitle}
+              onChange={(e) => setJobTitle(e.target.value)}
+            />
+            <FormField
+              label="Application link"
+              id="link"
+              type="url"
+              placeholder="Enter link to application..."
+              value={jobLink}
+              onChange={(e) => setJobLink(e.target.value)}
+            />
+            <FormField
+              label="Company name"
+              id="companyName"
+              type="text"
+              placeholder="Enter company name..."
+              value={companyName}
+              onChange={(e) => setCompanyName(e.target.value)}
+            />
+            <FormField
+              label="Location"
+              id="location"
+              type="text"
+              placeholder="Enter location..."
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+            <div className="flex flex-col mb-4">
+              <label
+                className="block font-bold text-headingColor mb-2 text-sm"
+                htmlFor="salaryAmount"
               >
-                <option value="">Select frequency</option>
-                <option value="hourly">Hourly</option>
-                <option value="monthly">Monthly</option>
-                <option value="yearly">Yearly</option>
-              </select>
+                Salary
+              </label>
+              <div className="flex">
+                <span className="font-bold text-xl self-center mr-1">$</span>
+                <input
+                  className="border border-borderprimary rounded w-full py-2 px-3 mr-2 text-primary leading-tight focus:outline-bluegray"
+                  id="salaryAmount"
+                  placeholder="0"
+                  type="number"
+                  min="0"
+                  max="1000000"
+                  value={salaryAmount}
+                  onChange={(e) => setSalaryAmount(e.target.value)}
+                  autoComplete="off"
+                />
+                <select
+                  className="border border-borderprimary text-xs bg-primary rounded w-fit py-1 px-1.5 text-primary leading-tight focus:outline-bluegray"
+                  id="salaryFrequency"
+                  value={salaryFrequency}
+                  onChange={(e) => setSalaryFrequency(e.target.value)}
+                >
+                  <option value="">Select frequency</option>
+                  <option value="hourly">Hourly</option>
+                  <option value="monthly">Monthly</option>
+                  <option value="yearly">Yearly</option>
+                </select>
+              </div>
             </div>
           </div>
-          <div className="flex flex-col mb-4">
-            <label
-              htmlFor="description"
-              className="block font-bold text-headingColor mb-2 text-sm"
-            >
-              Company details
-            </label>
-            <ReactQuill
-              id="companyDetails"
-              value={companyDetails}
-              onChange={setCompanyDetails}
-              modules={basicModules}
-              theme="snow"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label
-              htmlFor="description"
-              className="block font-bold text-headingColor mb-2 text-sm"
-            >
-              Job description
-            </label>
-            <ReactQuill
-              id="description"
-              value={jobDescription}
-              onChange={setJobDescription}
-              modules={basicModules}
-              theme="snow"
-            />
-          </div>
-          <div className="flex justify-center mt-6 gap-3">
-            <CreateFormButtonGroup handleCancel={() => handleModal(false)} />
+          <div className="w-1/2">
+            <div className="flex flex-col mb-4">
+              <label
+                htmlFor="description"
+                className="block font-bold text-headingColor mb-2 text-sm"
+              >
+                Company details
+              </label>
+              <ReactQuill
+                id="companyDetails"
+                value={companyDetails}
+                onChange={setCompanyDetails}
+                modules={basicModules}
+                theme="snow"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label
+                htmlFor="description"
+                className="block font-bold text-headingColor mb-2 text-sm"
+              >
+                Job description
+              </label>
+              <ReactQuill
+                id="description"
+                value={jobDescription}
+                onChange={setJobDescription}
+                modules={basicModules}
+                theme="snow"
+              />
+            </div>
+            <div className="flex justify-center mt-6 gap-3">
+              <CreateFormButtonGroup handleCancel={() => handleModal(false)} />
+            </div>
           </div>
         </form>
       </div>

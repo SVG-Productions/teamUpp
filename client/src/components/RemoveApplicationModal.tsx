a389bb9bf7d5cf25b8390a72e0fae92446f0b037
Gino Ochoa <gochoa1088@gmail.com>
2023-08-14 18:52:29 -0700

    create state and handle remove app handler

--- client/src/components/RemoveApplicationModal.tsx
+++ client/src/components/RemoveApplicationModal.tsx
@@ -1,15 +1,31 @@
 import React, { useState } from "react";
 import ModalLayout from "../layouts/ModalLayout";
+import { toast } from "react-hot-toast";
+import { basicToast } from "../utils/toastOptions";
+import axios from "axios";
 
 const RemoveApplicationModal = ({
-  handleDelete,
   handleModal,
+  teamId,
+  appId,
 }: {
-  handleDelete: () => void;
   handleModal: (bool: boolean) => void;
+  teamId: string;
+  appId: string;
 }) => {
-  const [isDeleting, setIsDeleting] = useState(false);
+  const [isRemoving, setIsRemoving] = useState(false);
 
+  const handleRemoveApplication = async () => {
+    try {
+      const response = await axios.delete(
+        `/api/teams/${teamId}/applications/${appId}`
+      );
+      toast.success(response.data.message, basicToast);
+    } catch (error: any) {
+      handleModal(false);
+      toast.error(error.response.data.message, basicToast);
+    }
+  };
   return (
     <ModalLayout handleClickOut={handleModal}>
       <div className="bg-primary w-full max-w-sm mx-auto p-4 z-10 sm:rounded-md sm:shadow-lg">
@@ -24,19 +40,19 @@ const RemoveApplicationModal = ({
           <button
             className="w-1/3 text-center min-w-[84px] text-sm bg-red-400 hover:bg-red-500 text-white 
           font-bold py-2 px-4 rounded-md focus:shadow-outline sm:w-1/4 sm:text-base"
-            onClick={() => {
-              setIsDeleting(true);
-              handleDelete();
+            onClick={async () => {
+              setIsRemoving(true);
+              await handleRemoveApplication();
             }}
-            disabled={isDeleting}
+            disabled={isRemoving}
           >
-            {isDeleting ? "Removing.." : "Remove"}
+            {isRemoving ? "Removing.." : "Remove"}
           </button>
           <button
             className="w-1/3 min-w-[84px] text-sm text-center bg-primary hover:bg-highlight border-2 
           text-primary font-bold py-2 px-4 rounded-md focus:shadow-outline sm:w-1/4 sm:text-base"
             onClick={() => handleModal(false)}
-            disabled={isDeleting}
+            disabled={isRemoving}
           >
             Cancel
           </button>

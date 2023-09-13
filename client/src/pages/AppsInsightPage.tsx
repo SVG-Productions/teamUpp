4be04a2ac362beedbd0d2d9756c26afe06069b88
vincentochoa3 <vochoa893@gmail.com>
2023-09-12 19:58:45 -0700

    add component for doughnut chart

--- client/src/pages/AppsInsightPage.tsx
+++ client/src/pages/AppsInsightPage.tsx
@@ -3,15 +3,21 @@ import React from "react";
 import { Params, useLoaderData } from "react-router-dom";
 import InsightsTotalCounts from "../components/InsightsTotalCounts";
 import InsightsLineGraph from "../components/InsightsLineGraph";
+import InsightsDoughnut from "../components/InsightsDoughnut";
 
 export const AppsInsightPage = () => {
   const { insightsData } = useLoaderData() as { insightsData: any };
 
   return (
-    <div>
+    <>
       <InsightsTotalCounts data={insightsData} />
-      <InsightsLineGraph insightsData={insightsData} />
-    </div>
+      <div>
+        <InsightsLineGraph insightsData={insightsData} />
+      </div>
+      <div>
+        <InsightsDoughnut />
+      </div>
+    </>
   );
 };
 
